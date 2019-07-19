0239bd13bd606da0002b7a6182eab784271ff874
marco <webgrafia@gmail.com>
2019-07-19 16:31:05 +0200

    Prima proposta f/e & b/e scheda progetto

--- template-parts/header/header-logged.php
+++ template-parts/header/header-logged.php
@@ -1,11 +1,14 @@
 <?php
 $current_user = wp_get_current_user();
 ?>
+<!--//
 <div class="header-notification-alert has-notifications d-none d-lg-block">
 	<a href="#">
 		<svg class="svg-bell-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bell-solid"></use></svg>
 	</a>
-</div><!-- /header-notification-alert -->
+</div> //-->
+
+<!-- /header-notification-alert -->
 <div class="header-user-logged d-none d-lg-block">
 	<a class="d-flex align-items-center dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 		<div class="avatar-wrapper">
@@ -21,17 +24,18 @@ $current_user = wp_get_current_user();
 				<div class="user-details-content">
 					<p><strong><?php echo $current_user->display_name; ?></strong></p>
 					<p><?php echo dsi_get_user_role($current_user); ?></p>
-					<a class="btn btn-action btn-xs" href="#">Crea e gestisci</a>
+					<a class="btn btn-action btn-xs" href="<?php echo get_edit_profile_url(); ?>">Crea e gestisci</a>
 				</div>
 			</div><!-- /user-details -->
 			<div class="menu-user-list">
 				<ul>
 					<li class="active">
-						<a href="#">
+						<a href="<?php echo admin_url(); ?>">
 							<span>Area personale</span>
 							<svg class="svg-home-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-home-solid"></use></svg>
 						</a>
 					</li>
+                    <!--
 					<li class="has-notifications">
 						<a href="#">
 							<span>Notifiche</span>
@@ -44,8 +48,9 @@ $current_user = wp_get_current_user();
 							<svg class="svg-bookmark-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bookmark-solid"></use></svg>
 						</a>
 					</li>
+					//-->
 					<li>
-						<a href="#">
+						<a href="<?php echo get_edit_profile_url(); ?>">
 							<span>Impostazioni profilo</span>
 							<svg class="svg-user-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-user-solid"></use></svg>
 						</a>
