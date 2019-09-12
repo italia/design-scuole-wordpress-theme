abc8d62125ce0782477e4f0f0b3264102bea7edf
marco <webgrafia@gmail.com>
2019-09-12 12:41:44 +0200

    css di stampa + fix actions

--- functions.php
+++ functions.php
@@ -182,6 +182,9 @@ function dsi_scripts() {
 
 	wp_enqueue_script( 'dsi-modernizr', get_template_directory_uri() . '/assets/js/modernizr.custom.js');
 
+	// print css
+    wp_enqueue_style('dsi-print-style',get_template_directory_uri() . '/print.css', array(),'20190912','print' );
+
 	// footer
 	wp_enqueue_script( 'dsi-boostrap-italia-js', get_template_directory_uri() . '/assets/js/bootstrap-italia.js', array(), false, false);
 	if(is_singular(array("servizio", "struttura", "luogo", "evento", "scheda_progetto", "post")) || is_archive() || is_search())
--- template-parts/single/actions.php
+++ template-parts/single/actions.php
@@ -1,69 +1,93 @@
-<div class="actions-wrapper actions-main">
-	<a class="toggle-actions" href="#" title="Vedi azioni" data-target="#modal-more-items" data-toggle="modal">
-		<svg class="it-more-items"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-more-items"></use></svg>
-		<span>Vedi azioni</span>
-	</a>
-	<div class="modal fade modal-actions" id="modal-more-items" tabindex="-1" role="dialog" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-				<div class="modal-body">
-					<div class="link-list-wrapper">
-						<ul class="link-list">
+<?php
+
+$parts = parse_url( home_url() );
+$current_uri = "{$parts['scheme']}://{$parts['host']}" . add_query_arg( NULL, NULL );
+if(is_singular()){
+    $current_title = get_the_title();
+}else if ( is_category() ) {
+    $current_title = __("Argomento", "design_scuole_italia").": ".single_cat_title( '', false );
+} elseif ( is_tag() ) {
+    $current_title = single_tag_title( '', false );
+} elseif ( is_tax("tipologia-articolo") ) {
+    $current_title = single_term_title('', false);
+} elseif ( is_tax("tipologia-servizio") ) {
+    $current_title = __("Servizi per ", "design_scuole_italia").": ".single_term_title('', false);
+} elseif ( is_post_type_archive() ) {
+    $current_title = post_type_archive_title('', false);
+}else{
+    $current_title = dsi_get_option("nome_scuola");
+}
+
+?><div class="actions-wrapper actions-main">
+    <a class="toggle-actions" href="#" title="Vedi azioni" data-target="#modal-more-items" data-toggle="modal">
+        <svg class="it-more-items"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-more-items"></use></svg>
+        <span><?php _e("Vedi azioni", "design_scuole_italia"); ?></span>
+    </a>
+    <div class="modal fade modal-actions" id="modal-more-items" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="link-list-wrapper">
+                        <ul class="link-list">
+                            <!--
 							<li>
 								<a href="#" class="list-item left-icon" title="Scarica il contenuto">
 									<svg class="icon it-download"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-download"></use></svg>
 									<span>Scarica</span>
 								</a>
 							</li>
-							<li>
-								<a href="#" class="list-item left-icon" title="Stampa il contenuto">
-									<svg class="icon it-print"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-print"></use></svg>
-									<span>Stampa</span>
-								</a>
-							</li>
+							//-->
+                            <li>
+                                <a href="javascript:window.print();" class="list-item left-icon" title="<?php _e("Stampa il contenuto", "design_scuole_italia"); ?>">
+                                    <svg class="icon it-print"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-print"></use></svg>
+                                    <span><?php _e("Stampa", "design_scuole_italia"); ?></span>
+                                </a>
+                            </li>
+                            <!--
 							<li>
 								<a href="#" class="list-item left-icon" title="Ascolta il contenuto">
 									<svg class="icon it-hearing"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-hearing"></use></svg>
 									<span>Ascolta</span>
 								</a>
 							</li>
-							<li>
-								<a href="#" class="list-item left-icon" title="Invia il contenuto">
-									<svg class="icon it-email"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-email"></use></svg>
-									<span>Invia</span>
-								</a>
-							</li>
-							<li>
-								<a class="list-item collapsed link-toggle" title="Menu Condividi" href="#social-share" data-toggle="collapse" aria-expanded="false" aria-controls="social-share">
-									<svg class="icon it-share"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-share"></use></svg>
-									<span>Condividi</span>
-									<svg class="icon icon-toggle svg-arrow-down-small"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-arrow-down-small"></use></svg>
-								</a>
-								<ul class="link-sublist collapse" id="social-share">
-									<li>
-										<a class="list-item" href="#" title="Condividi su: Facebook">
-											<svg class="icon it-social-facebook"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-facebook"></use></svg>
-											<span>Facebook</span>
-										</a>
-									</li>
-									<li>
-										<a class="list-item" href="#" title="Condividi su: Twitter">
-											<svg class="icon it-social-twitter"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-twitter"></use></svg>
-											<span>Twitter</span>
-										</a>
-									</li>
-									<li>
-										<a class="list-item" href="#" title="Condividi su: Linkedin">
-											<svg class="icon it-social-linkedin"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-linkedin"></use></svg>
-											<span>Linkedin</span>
-										</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</div>
-				</div><!-- /modal-body -->
-			</div><!-- /modal-content -->
-		</div><!-- /modal-dialog -->
-	</div><!-- /modal -->
-</div><!-- /actions-wrapper --><?php
+							//-->
+                            <li>
+                                <a href="mailto:?subject=<?php echo urlencode($current_title); ?>&body=<?php echo urlencode($current_uri); ?>" class="list-item left-icon" title="<?php _e("Invia il contenuto", "design_scuole_italia"); ?>">
+                                    <svg class="icon it-email"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-email"></use></svg>
+                                    <span><?php _e("Invia", "design_scuole_italia"); ?></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="list-item collapsed link-toggle" title="<?php _e("Condividi", "design_scuole_italia"); ?>" href="#social-share" data-toggle="collapse" aria-expanded="false" aria-controls="social-share">
+                                    <svg class="icon it-share"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-share"></use></svg>
+                                    <span><?php _e("Condividi", "design_scuole_italia"); ?></span>
+                                    <svg class="icon icon-toggle svg-arrow-down-small"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-arrow-down-small"></use></svg>
+                                </a>
+                                <ul class="link-sublist collapse" id="social-share">
+                                    <li>
+                                        <a class="list-item" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($current_uri); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Facebook" target="_blank">
+                                            <svg class="icon it-social-facebook"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-facebook"></use></svg>
+                                            <span>Facebook</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="list-item" href="http://twitter.com/share?text=<?php echo urlencode($current_title); ?>&url=<?php echo urlencode($current_uri); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Twitter"  target="_blank">
+                                            <svg class="icon it-social-twitter"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-twitter"></use></svg>
+                                            <span>Twitter</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="list-item" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo urlencode($current_uri); ?>&title=<?php echo urlencode($current_title); ?>&source=<?php echo dsi_get_option("nome_scuola"); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Linkedin"  target="_blank">
+                                            <svg class="icon it-social-linkedin"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-linkedin"></use></svg>
+                                            <span>Linkedin</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div><!-- /modal-body -->
+            </div><!-- /modal-content -->
+        </div><!-- /modal-dialog -->
+    </div><!-- /modal -->
+    </div><!-- /actions-wrapper --><?php
