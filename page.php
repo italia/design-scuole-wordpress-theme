fb81949f395d378841edc539bf6fe39884af4b77
marco <webgrafia@gmail.com>
2019-09-16 17:23:19 +0200

    fix page

--- page.php
+++ page.php
@@ -12,29 +12,57 @@ get_header();
 
     <main id="main-container" class="main-container">
 
-		<?php get_template_part("template-parts/common/breadcrumb"); ?>
-        <section class="section bg-white">
-            <div class="container ">
-                <article class="article-wrapper">
+        <?php get_template_part("template-parts/common/breadcrumb"); ?>
 
-                <?php
-		while ( have_posts() ) :
-			the_post();
+        <?php
+        while ( have_posts() ) :
+            the_post();
+            ?>
+            <section class="section bg-white">
+                <div class="container">
+                    <div class="row variable-gutters">
+                        <div class="col-md-12 article-title-author-container">
+                            <div class="title-content">
+                                <h1><?php the_title(); ?></h1>
+                            </div><!-- /title-content -->
+                        </div><!-- /col-md-6 -->
+                    </div><!-- /row -->
+                </div><!-- /container -->
+            </section>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+            <section class="section bg-white">
+                <div class="container ">
+                    <article class="article-wrapper">
 
-			the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+                        <div class="row variable-gutters">
+                            <div class="col-lg-12">
+                                <?php
+                                the_content();
+                                ?>
+                            </div>
+                        </div>
+                        <div class="row variable-gutters">
+                            <div class="col-lg-12">
+                                <?php
+                                if ( comments_open() || get_comments_number() ) :
+                                    comments_template();
+                                endif;
+                                ?>
+                            </div>
+                        </div>
+                        <div class="row variable-gutters">
+                            <div class="col-lg-12">
+                                <?php get_template_part( "template-parts/single/bottom" ); ?>
+                            </div><!-- /col-lg-9 -->
+                        </div><!-- /row -->
 
-		endwhile; // End of the loop.
-		?>
-                </article>
-            </div>
-        </section>
+                    </article>
+                </div>
+            </section>
+        <?php
+        endwhile; // End of the loop.
+        ?>
     </main><!-- #main -->
 
 
--- single.php
+++ single.php
@@ -64,7 +64,7 @@ get_header();
                                         ?>
 
                                         <?php
-                                        the_post_navigation();
+                                        //the_post_navigation();
 
                                         // If comments are open or we have at least one comment, load up the comment template.
                                         if ( comments_open() || get_comments_number() ) :
