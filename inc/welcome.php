754c5b167c69f0ee8cae0fac1c2135a1a89bebcd
marco <webgrafia@gmail.com>
2019-10-01 14:30:55 +0200

    Registrazione firma circolari + welcome dashboard + dashboard circolari utente

--- inc/welcome.php
+++ inc/welcome.php
@@ -112,7 +112,14 @@ function dsi_remove_all_dashboard_meta_boxes()
         }
     }
     $wp_meta_boxes['dashboard']['normal']['core'] = $keep_boxes;
-    $wp_meta_boxes['dashboard']['side']['core'] = array();
+
+    $keep_boxes = array();
+    foreach ($wp_meta_boxes['dashboard']['side']['core'] as $wp_meta_box) {
+        if (substr($wp_meta_box["id"], 0, 4) == "dsi_") {
+            $keep_boxes[] = $wp_meta_box;
+        }
+    }
+    $wp_meta_boxes['dashboard']['side']['core'] = $keep_boxes;
 }
 
 /**
