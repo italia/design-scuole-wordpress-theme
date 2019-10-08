8078919a93872f8c952215b43af59469ffc3e870
marco <webgrafia@gmail.com>
2019-10-08 16:20:07 +0200

    fix dettaglio bando

---
+++ single-avcp.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Design_Scuole_Italia
+ */
+global $post;
+get_header();
+
+?>
+    <main id="main-container" class="main-container">
+		<?php get_template_part("template-parts/common/breadcrumb"); ?>
+
+		<?php while ( have_posts() ) :  the_post(); ?>
+
+            <section class="section bg-white">
+                <div class="container">
+                    <div class="row variable-gutters">
+                        <div class="col-md-12 article-title-author-container">
+                            <div class="title-content">
+                                <h1><?php the_title(); ?></h1>
+                            </div><!-- /title-content -->
+                        </div><!-- /col-md-6 -->
+                    </div><!-- /row -->
+                </div><!-- /container -->
+            </section>
+            <section class="section bg-white">
+                <div class="container">
+                    <div class="row variable-gutters">
+                        <div class="col-lg-12">
+                            <article class="article-wrapper pt-4">
+                                <div class="row variable-gutters">
+                                    <div class="col-lg-12">
+                                        <?php
+                                        the_content();
+                                        ?>
+                                    </div><!-- /col-lg-8 -->
+                                </div><!-- /row -->
+                            </article>
+                        </div><!-- /col-lg-8 -->
+                    </div><!-- /row -->
+                </div><!-- /container -->
+            </section>
+
+
+			<?php get_template_part("template-parts/single/more-posts"); ?>
+
+            <?php  	endwhile; // End of the loop. ?>
+    </main><!-- #main -->
+<?php
+get_footer();
