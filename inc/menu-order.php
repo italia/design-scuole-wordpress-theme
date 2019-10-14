79299e24d82873bfb8cf6f2a202f22168a7860f8
marco <webgrafia@gmail.com>
2019-10-14 16:45:13 +0200

    fix evidenza menu admin per tassonomia classe

--- inc/menu-order.php
+++ inc/menu-order.php
@@ -1,6 +1,26 @@
 <?php
 
 
+add_action( 'parent_file', 'dsi_menu_highlight' );
+
+
+/**
+ * sistema evidenziazione menu in caso di selezione classe
+ * @param $parent_file
+ * @return string
+ */
+function dsi_menu_highlight( $parent_file ) {
+    global $current_screen;
+
+    $taxonomy = $current_screen->taxonomy;
+    if ( $taxonomy == 'classe' ) {
+        $parent_file = 'edit-tags.php?taxonomy=classe&post_type=programma_materia';
+    }
+
+    return $parent_file;
+}
+
+
 add_action('admin_menu','dsi_custom_admin_menu');
 function dsi_custom_admin_menu() {
    // swap_admin_menu_sections('Pagine','Articoli');              // Swap location of Posts Section with Pages Section
@@ -12,16 +32,20 @@ function dsi_custom_admin_menu() {
     rename_admin_menu_section("Easy Appointments", "Gestione Prenotazioni");
     swap_admin_menu_sections('Pagine','Gestione Prenotazioni');
     swap_admin_menu_sections('Media','Circolari');
+    //remove_submenu_page('edit.php?post_type=programma_materia', 'edit-tags.php?taxonomy=classe&amp;post_type=programma_materia');
 
-    $classe = add_menu_page(
-        'Classi',
-        'Classi',
-        'manage_options',
-        'edit-tags.php?taxonomy=classe&post_type=programma_materia',
-        '',
-        'dashicons-format-quote',
-        8
-    );
+
+        $classe = add_menu_page(
+            'Classi',
+            'Classi',
+            'manage_options',
+            'edit-tags.php?taxonomy=classe&post_type=programma_materia',
+            '',
+            'dashicons-format-quote',
+            8
+        );
+
+
     remove_admin_menu_item("edit.php?post_type=programma_materia",'edit-tags.php?taxonomy=classe&post_type=programma_materia');
 
 
