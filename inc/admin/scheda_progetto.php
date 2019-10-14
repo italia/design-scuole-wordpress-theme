5744e03a08237486db1734710f04ca8fef1354ee
marco <webgrafia@gmail.com>
2019-10-14 17:32:22 +0200

    aggiunti documenti a post type progetto

--- inc/admin/scheda_progetto.php
+++ inc/admin/scheda_progetto.php
@@ -231,7 +231,41 @@ function dsi_add_scheda_progetto_metaboxes() {
 		),
 	) );
 
-	$cmb_side = new_cmb2_box( array(
+
+    $cmb_undercontent->add_field( array(
+        'id' => $prefix . 'link_schede_documenti',
+        'name'    => __( 'Documenti', 'design_scuole_italia' ),
+        'desc' => __( 'Inserisci qui tutti i documenti che ritieni utili per descrivere il progetto. Se devi caricare il documento <a href="post-new.php?post_type=documento">puoi creare una breve scheda di presentazione</a> (soluzione consigliata e più efficace per gli utenti del sito) oppure caricarlo direttamente nei campi che seguono. ' , 'design_scuole_italia' ),
+        'type'    => 'custom_attached_posts',
+        'column'  => true, // Output in the admin post-listing as a custom column. https://github.com/CMB2/CMB2/wiki/Field-Parameters#column
+        'options' => array(
+            'show_thumbnails' => false, // Show thumbnails on the left
+            'filter_boxes'    => true, // Show a text box for filtering the results
+            'query_args'      => array(
+                'posts_per_page' => 10,
+                'post_type'      => 'documento',
+            ), // override the get_posts args
+        ),
+    ) );
+
+
+    $cmb_undercontent->add_field( array(
+        'id' => $prefix . 'file_documenti',
+        'name'    => __( 'Carica documenti', 'design_scuole_italia' ),
+        'desc' => __( 'Se l\'allegato non è descritto da una scheda documento, link all\'allegato (es. link a una locandina). ' , 'design_scuole_italia' ),
+        'type' => 'file_list',
+        // 'preview_size' => array( 100, 100 ), // Default: array( 50, 50 )
+        // 'query_args' => array( 'type' => 'image' ), // Only images attachment
+        // Optional, override default text strings
+        'text' => array(
+            'add_upload_files_text' => __('Aggiungi un nuovo Documento', 'design_scuole_italia' ), // default: "Add or Upload Files"
+            'remove_image_text' => __('Rimuovi Documento', 'design_scuole_italia' ), // default: "Remove Image"
+            'remove_text' => __('Rimuovi', 'design_scuole_italia' ), // default: "Remove"
+        ),
+    ) );
+
+
+    $cmb_side = new_cmb2_box( array(
 		'id'           => $prefix . 'box_side',
 		'title'        => __( 'Data Progetto', 'design_scuole_italia' ),
 		'object_types' => array( 'scheda_progetto' ),
--- single-scheda_progetto.php
+++ single-scheda_progetto.php
@@ -14,7 +14,8 @@ $is_luogo_scuola = dsi_get_meta("is_luogo_scuola");
 $link_schede_luoghi = dsi_get_meta("link_schede_luoghi");
 $nome_luogo_custom = dsi_get_meta("nome_luogo_custom");
 $link_strutture = dsi_get_meta("link_strutture");
-
+$link_schede_documenti = dsi_get_meta("link_schede_documenti");
+$file_documenti = dsi_get_meta("file_documenti");
 ?>
     <main id="main-container" class="main-container bluelectric">
 		<?php get_template_part("template-parts/common/breadcrumb"); ?>
@@ -78,6 +79,11 @@ $link_strutture = dsi_get_meta("link_strutture");
                                         <li>
                                             <a class="list-item scroll-anchor-offset" href="#art-par-risultati" title="Vai al paragrafo <?php _e("Risultati", "design_scuole_italia"); ?>"><?php _e("Risultati", "design_scuole_italia"); ?></a>
                                         </li>
+                                        <?php if((is_array($link_schede_documenti) && count($link_schede_documenti)>0) || (is_array($file_documenti) && count($file_documenti)>0)){ ?>
+                                            <li>
+                                                <a class="list-item scroll-anchor-offset" href="#art-par-documenti" title="<?php _e("Vai al paragrafo", "design_scuole_italia"); ?> <?php _e("Documenti", "design_scuole_italia"); ?>"><?php _e("Documenti", "design_scuole_italia"); ?></a>
+                                            </li>
+                                        <?php } ?>
                                         <li>
                                             <a class="list-item scroll-anchor-offset" href="#art-par-correlati" title="Vai al paragrafo <?php _e("Contenuti correlati", "design_scuole_italia"); ?>">Contenuti correlati<?php _e("", "design_scuole_italia"); ?></a>
                                         </li>
@@ -181,14 +187,49 @@ $link_strutture = dsi_get_meta("link_strutture");
                                     </div>
                                     </div>
                                 </div>
-									<?php }  ?>
+									<?php } ?>
                                 <?php if($post->post_content != ""){ ?>
+                                    <div class="row variable-gutters">
+                                        <div class="col-lg-9">
+                                            <h4 id="art-par-risultati"><?php _e("Risultati", "design_scuole_italia"); ?></h4>
+                                            <?php the_content(); ?>
+                                        </div>
+                                    </div>
+                                <?php }  ?>
+                                <?php if((is_array($link_schede_documenti) && count($link_schede_documenti)>0) || (is_array($file_documenti) && count($file_documenti)>0)){ ?>
+                                            <h4  id="art-par-documenti"><?php _e("Documenti", "design_scuole_italia"); ?></h4>
+                                            <div class="row variable-gutters">
+                                                <div class="col-lg-12">
+                                                    <div class="card-deck card-deck-spaced">
+                                                        <?php
+                                                        if(is_array($link_schede_documenti) && count($link_schede_documenti)>0) {
+                                                            global $documento;
+                                                            foreach ( $link_schede_documenti as $link_scheda_documento ) {
+                                                                $documento = get_post( $link_scheda_documento );
+                                                                get_template_part( "template-parts/documento/card" );
+                                                            }
+                                                        }
+
+                                                        global $idfile, $nomefile;
+                                                        if(is_array($file_documenti) && count($file_documenti)>0) {
+
+                                                            foreach ( $file_documenti as $idfile => $nomefile ) {
+                                                                get_template_part( "template-parts/documento/file" );
+                                                            }
+                                                        }
+
+                                                        ?>
+                                                    </div><!-- /card-deck card-deck-spaced -->
+                                                </div><!-- /col-lg-12 -->
+                                            </div><!-- /row -->
+                                            <?php
+                                        }
+
+                                        ?>
+
                                 <div class="row variable-gutters">
                                     <div class="col-lg-9">
-                                <h4 id="art-par-risultati"><?php _e("Risultati", "design_scuole_italia"); ?></h4>
-                                <?php the_content(); ?>
-                                <?php }  ?>
-                                <?php get_template_part("template-parts/single/bottom"); ?>
+                                        <?php get_template_part("template-parts/single/bottom"); ?>
                                     </div>
                                 </div>
                             </article>
