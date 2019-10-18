b738a096480b4ab407a11fa73e889965abde8edb
marco <webgrafia@gmail.com>
2019-10-18 13:48:11 +0200

    aggiunta stampa filetype documento

--- template-parts/documento/file.php
+++ template-parts/documento/file.php
@@ -11,17 +11,19 @@ if(substr($nomefile, -4) == "docx")
 if(substr($nomefile, -3) == "xml")
 	$icon = "svg-xml-document";
 
+
 $attach = get_post($idfile);
 $filetocheck = get_attached_file($idfile);
 
 $filesize = filesize($filetocheck);
+$type = mime_content_type($filetocheck);
 ?>
 	<div class="card card-bg card-icon rounded">
 		<div class="card-body">
 			<svg class="icon <?php echo $icon; ?>"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#<?php echo $icon; ?>"></use></svg>
 			<div class="card-icon-content">
 				<p><strong><a target="_blank" href="<?php echo $attach->guid; ?>"><?php echo $attach->post_title; ?></a></strong></p>
-				<small><?php echo intval($filesize/1024); ?> kb</small>
+				<small><?php echo $type; ?> <?php echo intval($filesize/1024); ?> kb</small>
 			</div><!-- /card-icon-content -->
 		</div><!-- /card-body -->
 	</div><!-- /card card-bg card-icon rounded -->
