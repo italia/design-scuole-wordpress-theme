77456198eedd26c617281e65357f6db277359703
marco <webgrafia@gmail.com>
2019-11-04 14:21:09 +0100

    fix card servizio

--- template-parts/servizio/card.php
+++ template-parts/servizio/card.php
@@ -9,7 +9,7 @@ global $servizio;
 			<svg class="icon svg-marker-simple"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-service"></use></svg>
 			<div class="card-icon-content">
 				<p><strong><?php echo $servizio->post_title; ?></strong></p>
-				<small><?php  echo dsi_get_meta("sottotitolo" , '_dsi_servizio_', $servizio->ID); ?></small>
+				<small><?php  echo dsi_get_meta("descrizione" , '_dsi_servizio_', $servizio->ID); ?></small>
 			</div><!-- /card-icon-content -->
 		</div><!-- /card-body -->
 	</a>
