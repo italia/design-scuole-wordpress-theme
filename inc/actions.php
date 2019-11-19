1de89b67c00a2979891532642d77ea8cb9bf327e
marco <webgrafia@gmail.com>
2019-11-19 10:25:14 +0100

    fix admin multisite

--- inc/actions.php
+++ inc/actions.php
@@ -543,3 +543,58 @@ function dsi_before_admin_bar_render()
 
     $wp_admin_bar->remove_menu('customize');
 }
+
+
+/**
+ * abilito edit utenti agli admin di un netork multisite
+ */
+
+function dsi_admin_users_caps( $caps, $cap, $user_id, $args ){
+
+    foreach( $caps as $key => $capability ){
+
+        if( $capability != 'do_not_allow' )
+            continue;
+
+        switch( $cap ) {
+            case 'edit_user':
+            case 'edit_users':
+                $caps[$key] = 'edit_users';
+                break;
+            case 'delete_user':
+            case 'delete_users':
+                $caps[$key] = 'delete_users';
+                break;
+            case 'create_users':
+                $caps[$key] = $cap;
+                break;
+        }
+    }
+
+    return $caps;
+}
+add_filter( 'map_meta_cap', 'dsi_admin_users_caps', 1, 4 );
+remove_all_filters( 'enable_edit_any_user_configuration' );
+add_filter( 'enable_edit_any_user_configuration', '__return_true');
+
+/**
+ * Checks that both the editing user and the user being edited are
+ * members of the blog and prevents the super admin being edited.
+ */
+function dsi_edit_permission_check() {
+    global $current_user, $profileuser;
+
+    $screen = get_current_screen();
+
+    get_currentuserinfo();
+
+    if( ! is_super_admin( $current_user->ID ) && in_array( $screen->base, array( 'user-edit', 'user-edit-network' ) ) ) { // editing a user profile
+        if ( is_super_admin( $profileuser->ID ) ) { // trying to edit a superadmin while less than a superadmin
+            wp_die( __( 'You do not have permission to edit this user.' ) );
+        } elseif ( ! ( is_user_member_of_blog( $profileuser->ID, get_current_blog_id() ) && is_user_member_of_blog( $current_user->ID, get_current_blog_id() ) )) { // editing user and edited user aren't members of the same blog
+            wp_die( __( 'You do not have permission to edit this user.' ) );
+        }
+    }
+
+}
+add_filter( 'admin_head', 'dsi_edit_permission_check', 1, 4 );
