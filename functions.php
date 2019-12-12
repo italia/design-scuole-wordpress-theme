f663aa495266c28cb9dcaa1328193c3f2b9915b8
marco <webgrafia@gmail.com>
2019-12-12 11:44:49 +0100

    aggiungo mappa a indirizzo

--- functions.php
+++ functions.php
@@ -223,7 +223,7 @@ function dsi_scripts() {
 
 	// footer
 	wp_enqueue_script( 'dsi-boostrap-italia-js', get_template_directory_uri() . '/assets/js/bootstrap-italia.js', array(), false, true);
-	if(is_singular(array("servizio", "struttura", "luogo", "evento", "scheda_progetto", "post", "circolare")) || is_archive() || is_search() || is_post_type_archive("luogo"))
+	if(is_singular(array("servizio", "struttura", "luogo", "evento", "scheda_progetto", "post", "circolare", "indirizzo")) || is_archive() || is_search() || is_post_type_archive("luogo"))
 		wp_enqueue_script( 'dsi-leaflet-js', get_template_directory_uri() . '/assets/js/components/leaflet/leaflet.js', array(), false, false);
 /*
 	if(is_singular()){
