c218fc1b3d01b73ff612c41115534e93c80bc57c
marco <webgrafia@gmail.com>
2019-12-13 22:58:17 +0100

    aggiunto controllo stato

--- template-parts/servizio/card.php
+++ template-parts/servizio/card.php
@@ -1,17 +1,19 @@
 <?php
 global $servizio;
-
-?>
-
-<div class="card card-bg card-icon rounded">
-	<a href="<?php echo get_permalink($servizio); ?>">
-		<div class="card-body">
-			<svg class="icon svg-marker-simple"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-service"></use></svg>
-			<div class="card-icon-content">
-				<p><strong><?php echo $servizio->post_title; ?></strong></p>
-				<small><?php  echo dsi_get_meta("descrizione" , '_dsi_servizio_', $servizio->ID); ?></small>
-			</div><!-- /card-icon-content -->
-		</div><!-- /card-body -->
-	</a>
-</div><!-- /card card-bg card-icon rounded -->
-<?php
+if($servizio->post_status == "publish") {
+    ?>
+    <div class="card card-bg card-icon rounded">
+        <a href="<?php echo get_permalink($servizio); ?>">
+            <div class="card-body">
+                <svg class="icon svg-marker-simple">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-service"></use>
+                </svg>
+                <div class="card-icon-content">
+                    <p><strong><?php echo $servizio->post_title; ?></strong></p>
+                    <small><?php echo dsi_get_meta("descrizione", '_dsi_servizio_', $servizio->ID); ?></small>
+                </div><!-- /card-icon-content -->
+            </div><!-- /card-body -->
+        </a>
+    </div><!-- /card card-bg card-icon rounded -->
+    <?php
+}
