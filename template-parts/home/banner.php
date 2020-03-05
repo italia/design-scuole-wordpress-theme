d039bf2e9047e87b33eb5bd59091e897c234c7f5
marco <webgrafia@gmail.com>
2020-03-05 22:32:09 +0100

    fix posizionamento banner singolo

--- template-parts/home/banner.php
+++ template-parts/home/banner.php
@@ -3,14 +3,16 @@
 $visualizza_banner = dsi_get_option("visualizza_banner", "homepage");
 if($visualizza_banner == "si") {
     $banner_group = dsi_get_option("banner_group", "homepage");
-
+    $class = "single-banner";
+    if(count($banner_group) > 1)
+        $class = "owl-carousel carousel-theme carousel-simple";
     ?>
     <section class="section bg-gray-light py-3">
         <div class="container py-2">
             <div class="row variable-gutters">
                 <div class="col">
                     <p class="text-center"><?php _e("Pubblicità e Informazione", "design_scuole_italia"); ?></p>
-                    <div class="owl-carousel carousel-theme carousel-simple">
+                    <div class="<?php echo $class; ?>">
                         <?php
                         foreach ($banner_group as $banner){
                             $image_url = wp_get_attachment_image_url($banner["banner_id"], 'banner' );
