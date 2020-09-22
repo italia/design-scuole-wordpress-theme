d6e2c2b9235772ccd79ead2ac79c864a6b4663df
marco <spappolo>
2020-09-22 23:19:02 +0200

    Fix accessibilità

--- template-parts/autore/card.php
+++ template-parts/autore/card.php
@@ -2,7 +2,7 @@
 global $autore;
 ?>
 <div class="card-avatar-img">
-    <a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><img src="<?php echo dsi_get_user_avatar($autore); ?>"></a>
+    <a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><img src="<?php echo dsi_get_user_avatar($autore); ?>" alt="<?php echo esc_attr(dsi_get_display_name( $autore->ID )); ?>"></a>
 </div><!-- /card-avatar-img -->
 <div class="card-avatar-content">
     <p class="font-weight-normal">da <strong class="text-underline"><u><a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><?php echo dsi_get_display_name( $autore->ID ); ?></a></u></strong></p>
