8abbd7b6e9c87633e672e0a585503c01ba8874fa
Marco Buttarini <webgrafia@gmail.com>
2021-09-06 14:52:03 +0200

    Update card.php

--- template-parts/luogo/card.php
+++ template-parts/luogo/card.php
@@ -11,22 +11,24 @@ if($lg == 9){
     $col2=6;
 }
 
+$card_title = $luogo->post_title;
+$indirizzo = dsi_get_meta("indirizzo", '_dsi_luogo_', $luogo->ID);
+$posizione_gps = dsi_get_meta("posizione_gps", '_dsi_luogo_', $luogo->ID);
+$cap = dsi_get_meta("cap", '_dsi_luogo_', $luogo->ID);
+$mail = dsi_get_meta("mail", '_dsi_luogo_', $luogo->ID);
+$telefono = dsi_get_meta("telefono", '_dsi_luogo_', $luogo->ID);
+
 // controllo se è un parent, in caso recupero i dati del genitore
 if($luogo->post_parent == 0){
-	$card_title = $luogo->post_title;
-	$indirizzo = dsi_get_meta("indirizzo", '_dsi_luogo_', $luogo->ID);
-	$posizione_gps = dsi_get_meta("posizione_gps", '_dsi_luogo_', $luogo->ID);
-	$cap = dsi_get_meta("cap", '_dsi_luogo_', $luogo->ID);
-	$mail = dsi_get_meta("mail", '_dsi_luogo_', $luogo->ID);
-	$telefono = dsi_get_meta("telefono", '_dsi_luogo_', $luogo->ID);
+	
 }else{
-    $parent = get_post($luogo->post_parent);
+	$parent = get_post($luogo->post_parent);
 	$card_title = $parent->post_title;
-	$indirizzo = dsi_get_meta("indirizzo", "_dsi_luogo_", $luogo->post_parent);
-	$posizione_gps = dsi_get_meta("posizione_gps", "_dsi_luogo_", $luogo->post_parent);
-	$cap = dsi_get_meta("cap", "_dsi_luogo_", $luogo->post_parent);
-	$mail = dsi_get_meta("mail", "_dsi_luogo_", $luogo->post_parent);
-	$telefono = dsi_get_meta("telefono", "_dsi_luogo_", $luogo->post_parent);
+	if(!$indirizzo)	$indirizzo = dsi_get_meta("indirizzo", "_dsi_luogo_", $luogo->post_parent);
+	if(!$posizione_gps) $posizione_gps = dsi_get_meta("posizione_gps", "_dsi_luogo_", $luogo->post_parent);
+	if(!$cap) $cap = dsi_get_meta("cap", "_dsi_luogo_", $luogo->post_parent);
+	if(!$mail) $mail = dsi_get_meta("mail", "_dsi_luogo_", $luogo->post_parent);
+	if(!$telefono) $telefono = dsi_get_meta("telefono", "_dsi_luogo_", $luogo->post_parent);
 }
 
 $orario_pubblico = dsi_get_meta("orario_pubblico", '_dsi_luogo_', $luogo->ID);
