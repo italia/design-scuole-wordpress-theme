f3663074d4136827b96f2a734b791533ac1c0b3a
Lorenzo Dalla Muta <lorenzo.dalla.muta@gmail.com>
2022-04-20 12:26:45 +0000

    archive-documento: restyle

--- archive-documento.php
+++ archive-documento.php
@@ -34,31 +34,26 @@ global $documento;
                             $classcolor = "bg-gray-light";
 
                         ?>
-                        <section class="section <?php echo $classcolor; ?> py-4">
+                        <section class="section <?php echo $classcolor; ?> py-5">
                             <div class="container">
                                 <?php
                                 if (is_array($documenti) && count($documenti) > 0) {
                                     ?>
-
-                                    <div class="row variable-gutters mt-4">
-                                        <div class="col-lg-10 offset-lg-1 mb-4">
-                                            <h4 class="text-left mb-3">
-                                                <a href="<?php echo get_term_link($tipologia_documento); ?>"><?php if (count($documenti) > 1) echo dsi_pluralize_string($tipologia_documento->name); else echo $tipologia_documento->name; ?></a>
-                                            </h4>
-                                        </div><!-- /col-lg-3 -->
-                                        <?php
-
-                                        ?>
-                                        <div class="col-lg-10 offset-lg-1">
-                                            <div class="row variable-gutters">
-                                                <?php foreach ($documenti as $documento) { ?>
-                                                    <div class="col-lg-4 mb-4">
-                                                        <?php get_template_part("template-parts/documento/card", "ico"); ?>
-                                                    </div><!-- /col-lg-4 -->
-                                                <?php } ?>
-                                            </div><!-- /row -->
-                                        </div><!-- /col-lg-9 -->
+                                    <div class="title-section mb-5">
+                                        <h2 class="h4">
+                                            <?php if (count($documenti) > 1) echo dsi_pluralize_string($tipologia_documento->name); else echo $tipologia_documento->name; ?>
+                                        </h2>
+                                    </div><!-- /title-section -->
+                                    <div class="row variable-gutters">
+                                        <?php foreach ($documenti as $documento) { ?>
+                                            <div class="col-lg-4 mb-3 mb-lg-4">
+                                                <?php get_template_part("template-parts/documento/card", "ico"); ?>
+                                            </div><!-- /col-lg-4 -->
+                                        <?php } ?>
                                     </div><!-- /row -->
+                                    <div class="pt3 text-center">
+                                        <a class="text-underline" href="<?php echo get_term_link($tipologia_documento) ?>"><strong><?php _e("Vedi tutti", "design_scuole_italia"); ?></strong></a>
+                                    </div>
                                     <?php
                                 }
                                 ?>
