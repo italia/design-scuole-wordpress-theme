31181f509b0caad39f10282a5974269e3b69aec8
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2022-05-12 08:42:16 +0000

    ordine sezioni in presentazione scuola

--- page-templates/la-scuola.php
+++ page-templates/la-scuola.php
@@ -20,8 +20,8 @@ get_header();
 			get_template_part("template-parts/home/timeline");
 			get_template_part("template-parts/home/strutture");
 //			get_template_part("template-parts/home/commissioni");
-			get_template_part("template-parts/home/gallery-luoghi");
-			get_template_part("template-parts/home/le-carte");
+            get_template_part("template-parts/home/le-carte");
+            get_template_part("template-parts/home/gallery-luoghi");
 			get_template_part("template-parts/home/i-numeri");
 
 		endwhile; // End of the loop.
