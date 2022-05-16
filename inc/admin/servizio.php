0de7f6d08b7bfb9f8f5ccbaed6f072ecf720b391
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2022-05-16 11:02:47 +0000

    fix checkbox autenticazione x servizi

--- inc/admin/servizio.php
+++ inc/admin/servizio.php
@@ -251,26 +251,25 @@ function dsi_add_servizi_metaboxes() {
 	) );
 
 	$cmb_undercontent->add_field( array(
-		'id' => $prefix . 'modalita_autenticazione',
-		'name' => __( 'Modalità di autenticazione', 'design_scuole_italia' ),
-		'desc' => __( 'Selezionare se sono previste modalità di autenticazione per accedere al servizio.' , 'design_scuole_italia' ),
-		'type' => 'checkbox',
-	) );
-
-	$cmb_undercontent->add_field(array(
-		'id' => $prefix . 'provider_autenticazione',
-		'name' => __( 'Provider di autenticazione', 'design_scuole_italia' ),
-		'desc' => __( 'Selezionare i provider di autenticazione tra SPID e Carta di Identità Elettronica (CIE).' , 'design_scuole_italia' ),
-		'type' => 'pw_multiselect',
-		'options' => array(
-			'SPID' => 'SPID',
-			'CIE' => 'CIE'
-		),
-		'attributes'    => array(
-			'data conditional id'     => $prefix.'modalita_autenticazione',
-			'data conditional value'  => "false",
-		),
-	) );
+        'id'   => $prefix . 'modalita_autenticazione',
+        'name' => __( 'Modalità di autenticazione', 'design_scuole_italia' ),
+        'desc' => __( 'Selezionare se sono previste modalità di autenticazione per accedere al servizio.' , 'design_scuole_italia' ),
+        'type' => 'checkbox',
+    ) );
+    $cmb_undercontent->add_field(array(
+        'id' => $prefix . 'provider_autenticazione',
+        'name'        => __( 'Provider di autenticazione', 'design_scuole_italia' ),
+        'desc' => __( 'Selezionare i provider di autenticazione tra SPID e Carta di Identità Elettronica (CIE).' , 'design_scuole_italia' ),
+        'type' => 'pw_multiselect',
+        'options' => array(
+            'SPID' => 'SPID',
+            'CIE' => 'CIE'
+        ),
+        'attributes'    => array(
+            'data-conditional-id'     => $prefix.'modalita_autenticazione',
+            'data-conditional-value'  => "false",
+        ),
+    ) );
 
     $cmb_undercontent->add_field( array(
         'id' => $prefix . 'struttura_responsabile',
