350a50ba3b3de40102fb0adff157fa9884ea8127
scanavino.marcello <scanavino.marcello@accenture.com>
2022-06-13 15:15:07 +0200

    fix id argomenti

--- template-parts/search/filters.php
+++ template-parts/search/filters.php
@@ -57,7 +57,7 @@ if(isset($_GET["post_terms"]))
     }
     ?>
         <h3 class="h6 text-uppercase"><strong><?php _e("Argomenti", "design_scuole_italia"); ?></strong></h3>
-        <ul>
+        <ul id="lista-argomenti">
             <?php
             $terms = get_terms( array(
 	            'taxonomy' => 'post_tag',
