3083c3b317a714ae3684fd2eab9bb8876ff60315
Alessandro Magnaguagno <alessandro.magnaguagno@artigianelli.tn.it>
2022-08-08 16:11:46 +0200

    pagine icdl - attività linguistiche e correzioni

---
+++ page-ICDL-full-standard.php
@@ -0,0 +1,306 @@
+<?php
+/* Template Name: ICDL full standard
+ *
+ * didattica template file
+ *
+ * @package Design_Scuole_Italia
+ */
+
+get_header();
+
+
+?>
+
+<main id="main-container" class="main-container">
+       
+      
+       <?php get_template_part("template-parts/hero/hero_page"); ?> 
+
+
+    <section id="text-block" class="section bg-white">
+        <div class="container-fluid container-border-top">
+                <div class="row main-content variable-gutters">
+
+                    <div class="container col-lg-8 col-xxl-6">
+
+                        <div class="row"> 
+                            <div class="pt-5 px-3">    
+                                <h2>ICDL full standard</h2>
+                                <p>lI 1° aprile 2014 ICDL Full Standard, che oggi si chiama ICDL Full Standard, è stata accreditata come schema di certificazione delle competenze informatiche da Accredia, unico ente italiano di accreditamento preposto a validare i processi di certificazione delle persone.
+                                </p>
+                                
+                                <h3>Perchè certificarsi?</h3>
+                                <p>Nelle Scuole secondarie di secondo grado e in molti corsi di Laurea e dipartimenti Universitari ai possessori di una certificazione ICDL viene riconosciuto un credito formativo.
+                                <br>
+                                Nel mondo del lavoro costituisce un valore aggiunto con cui poter arricchire il proprio curriculum. 
+                                <br>
+                                Nella Pubblica Amministrazione, l'ICDL fa punteggio o è prerequisito in molti concorsi a titoli ed esami e in bandi di assunzione.</p>
+
+                                <h3>Quali certificazioni?</h3>
+                                <p>Nelle Scuole secondarie di secondo grado e in molti corsi di Laurea e dipartimenti Universitari ai possessori di una certificazione ICDL viene riconosciuto un credito formativo.
+                                <br>
+                                Nel mondo del lavoro costituisce un valore aggiunto con cui poter arricchire il proprio curriculum.
+                                <br>
+                                Nella Pubblica Amministrazione, l'ICDL fa punteggio o è prerequisito in molti concorsi a titoli ed esami e in bandi di assunzione.</p>
+                            </div>
+                        </div>
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Base</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p><strong>Composta da 4 moduli base con percorso obbligatorio:</strong></p>
+                                <ul>
+                                    <li>Computer Essentials</li>
+                                    <li>Online Essentials</li>
+                                    <li>Word Processing</li>
+                                    <li>Spreadsheets</li>
+                                </ul>
+                            </div>
+                        </div> <!-- scheda titoletto + elenco -->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Standard</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p><strong>Composta dai 4 moduli base + 3 a scelta fra quelli elencati:</strong></p>
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <p>Moduli a scelta:</p>
+                                        <ul>
+                                            <li>Computer Essentials</li>
+                                            <li>Online Essentials</li>
+                                            <li>Word Processing</li>
+                                            <li>Spreadsheets</li>
+                                        </ul>
+                                    </div>
+                                    <div class="offset-sm-2 col-sm-5">
+                                        <p>Moduli a scelta:</p>
+                                        <ul>
+                                            <li>Online Collaboration</li>
+                                            <li>Using Database</li>
+                                            <li>Presentation</li>
+                                            <li>IT Security</li>
+                                            <li>Project Planning</li>
+                                            <li>WebEditing</li>
+                                            <li>ImageEditing</li>
+                                            <li>CAD 2D</li>
+                                            <li>Health/li>
+                                            <li>Digital Marketing</li>
+                                            <li>Computing</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> <!-- scheda titoletto + elenco -->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Full Standard</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p><strong>Composta da 4 moduli base con percorso obbligatorio:</strong></p>
+                                <ul>
+                                    <li>CComputer Essentials</li>
+                                    <li>Online Essentials</li>
+                                    <li>Word Processing</li>
+                                    <li>Spreadsheets</li>
+                                    <li>Online Collaboration</li>
+                                    <li>Presentation</li>
+                                    <li>IT Security</li>
+                                </ul>
+                            </div>
+                        </div> <!-- scheda titoletto + elenco -->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Advanced Word Processing</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>Si ottiene superando il solo modulo d'esame Advanced Word Processing (correzione automatica o manuale)</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Advanced Spreadsheets</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>Si ottiene superando il solo modulo d'esame Advanced Spreadsheets (correzione automatica o manuale)</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                         <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Advanced Databases</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>SSi ottiene superando il solo modulo d'esame Advanced Databases (correzione automatica o manuale)</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Advanced Presentation</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>Si ottiene superando il solo modulo d'esame Advanced Presentation (correzione automatica o manuale)</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL Expert</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>Composta da 3 moduli Advanced a scelta</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL CAD 2D</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>Si ottiene superando il solo modulo d'esame CAD 2D</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                        <div class="row pt-5 px-3">
+                            <div class="col-lg-4">
+                                <h4>ICDL CAD 2D</h4>
+                            </div>
+                            <div class="offset-lg-1 col-lg-7">
+                                <p>Si ottiene superando il solo modulo d'esame CAD 2D</p>
+                            </div>
+                        </div> <!-- scheda titoletto + paragrafo-->
+
+                        <!-- Secondo me sta roba però deve essere editabile -->
+
+
+                        
+                        <div class="row">
+                            <div class="pt-5 px-3">    
+                                <h3>Costi</h3>
+                                <p>Il costo per il rilascio da parte dell'AICA delle certificazioni ICDL è formato da 2 voci:</p>
+                                    <ul>
+                                        <li>il costo per l'acquisto della Skills Card (iscrizione al programma di esami ICDL);</li>
+                                        <li>il costo per l'iscrizione a ciascun esame.</li>
+                                   </ul>
+                                   <p>I prezzi praticati dall’Istituto Martini nell’a.s. 2021/2022 sono riportati nella seguente tabella:</p>
+                            </div>
+                                <div class="col-12 text-center">
+                                        <img class="img-mobile" src="<?php echo get_template_directory_uri () ?>/assets/placeholders/img-placeholder-500x384.jpg" alt="">
+                                </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="pt-5 px-3">    
+                                <h4>Esami IT Security “Io clicco sicuro” con la carta “IoStudio”</h4>
+                                <p>Iocliccosicuro è un’iniziativa di AICA e di ioStudio - la Carta dello Studente, che ti permette di sostenere gratuitamente il modulo IT-Security di ICDL 
+                                Se hai IoStudio - la Carta dello Studente, puoi sfruttare questa opportunità, dal sito 
+                                <a href="blank">IoStudio la carta dello studente.</a>
+                                </p>
+                                <p>Per iscriverti segui questi passaggi:
+                                    Se non l’hai già fatto, ricordati di attivare il tuo account sul Portale dello Studente cliccando su “PRIMO ACCESSO” 
+                                    Fai login con la tua Utenza e Password e seleziona tra le offerte la categoria “Corsi di Informatica”. 
+                                </p>
+                                <p>Scegli il progetto “iocliccosicuro - con ICDL puoi”.
+                                Clicca sul pulsante VAI. Compila il form e segui le istruzioni per prepararti su micertificoICDL.it
+                                Preparati sulla piattaforma di formazione online <a href="">www.micertificoICDL.it</a> Da quando ti registri qui, hai 12 mesi per studiare e, quando ti sentirai pronto, sostenere l’esame. 
+                                </p>
+                                <h6>Link per la nuova procedura di attivazione del progetto </h6>
+                                <p>Per ulteriori informazioni 
+                                <a href="">https://www.aicanet.it/iocliccosicuro</a> </p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="pt-5 px-3">    
+                                <h3>Informazioni e link utili</h3>
+                                <p>Possedendo una Skills Card Nuova ICDL, ci si può iscrivere ad una sessione di esami programmata da qualsivoglia Test Center. 
+                                Per iscriversi a un esame non è necessario stampare la Skills Card, essendo un documento digitale registrato nel sistema informatico di AICA, è sufficiente riportare il numero sul modulo di iscrizione.
+                                Il pagamento della quota di esame dà diritto a effettuare l'esame solo presso il Test Center al quale è stata pagata.
+                                L'iscrizione deve essere eseguita con un preavviso superiore ai 5 giorni rispetto alla data della Sessione e, comunque, sulla base delle indicazioni del Test Center.
+                                Il candidato può decidere di sostenere gli esami in Test Center differenti.
+                                È possibile effettuare un versamento cumulativo di skills card ed esami.
+                                Il possessore di una Skills card può accedere via web alla Card intestatagli, registrandosi sul sito di AICA, nella sezione ad accesso riservato denominata Linea diretta per il candidato, prendendo così visione dei propri dati anagrafici e degli esiti degli esami superati.
+                                </p>
+                                <p><strong>NOTA BENE:</strong> Coloro che possiedono una Skills card acquistata prima del 2014 possono richiedere il passaggio alla Nuova ICDL acquistando una nuova Skills card su cui gli esami sostenuti verranno convertiti, previo un esame UPDATE, nel seguente modo:</p>
+                            </div>
+                        </div>
+
+                            <div class="row">
+                                <div class="col-12 text-center">
+                                    <img class="img-mobile" src="<?php echo get_template_directory_uri () ?>/assets/placeholders/img-placeholder-500x384.jpg" alt="">
+                                </div>
+                            </div>
+
+                        <div class="row">
+                            <div class="pt-5 px-3">    
+                                <h5>Link utili</h5>
+                                <p>
+                                <a href=""> www.ICDL.it </a> / <a href=""> www.aica.it </a> / <a href=""> www.micertificoICDL.it </a> / <a href=""> www.aula01.it</a> 	
+                                <br>  		   
+                                <br>
+                                    N.B.: Alcune sezioni dei siti riportati qui di seguito sono consultabili solo previa registrazione alla piattaforma e ottenimento di credenziali da parte del gestore (gratuitamente o a pagamento). 
+                                    <br>
+                                    <br>
+                                    <a href="">Introduzione Syllabus 6.0 </a>
+                                </p>
+                            </div>
+                        </div>
+
+
+                        
+                        
+
+
+                    </div>  <!-- Main content of page -->
+                    
+                    <div id="sidebar" class="col-lg-3 offset-lg-1 col-xxl-4 offset-xxl-2 px-3 py-5">
+                        <aside class="aside-main aside-sticky">
+                            <div class="aside-title col-10 col-xl-8" id="program-legend">
+                                <h5>Contatti</h5>
+                                <p id="quotes">È necessario contattare la dottoressa Fontana tramite email o telefono per concordare data e orario dell’appuntamento. In caso di necessità particolari, è possibile concordare l’appuntamento in altre date e orari. </p>
+                            </div>
+                            <div class="col-lg-3 offset-lg-1 col-xl-4 offset-xl-2 aside-border px-3 py-4">
+                            <?php 
+    
+                                        if(trim($procedura_esito) != ""){ 
+                                            ?> 
+                                            <h6 class="h6"><?php _e("email", "design_scuole_italia"); ?></h6> 
+                                            <div class="row variable-gutters"> 
+                                                <div class="col-lg-9"> 
+                                                    <?php echo wpautop($procedura_esito); ?> 
+                                                </div><!-- /col-lg-9 --> 
+                                            </div><!-- /row --> 
+                                            <?php 
+                                        } 
+                                        ?>
+
+                            </div>
+                            <div id="lista-paragrafi" class="link-list-wrapper" role="region"
+                                aria-labelledby="program-legend"> 
+                                <ul class="link-list">
+                                    <h6>prova di un sottotitolo</h6>
+                                </ul>
+                            </div>
+                        </aside>
+                    </div>
+                    
+                    
+                </div><!-- /row -->
+        </div><!-- /container -->
+    </section>
+
+
+
+</main>
+
+
+
+<?php
+get_footer();
