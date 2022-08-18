fb40a6c9d2036f98d8cc3a7e6db9096485f98d2a
paolosartori <108754914+paolosartori@users.noreply.github.com>
2022-08-18 18:01:20 +0200

    Responsive home 1

--- front-page.php
+++ front-page.php
@@ -14,7 +14,7 @@ get_header();
     </section><!--#hero -->
     
     <!-- PRESENTAZIONE SCUOLA -->
-    <section id="presentazione-scuola" class="container"> 
+    <section id="presentazione-scuola" class="container mt-5"> 
       <div class="row">
         <div id="container-presentazione" class="col-12 col-lg-8">
           <div id="nostro-istituto">
@@ -36,7 +36,7 @@ get_header();
 
         <div id="sidebar-presentazione" class="offset-lg-1 col-12 col-lg-3 order-1 order-lg-0">
           <h4 class="d-lg-none">Area Docenti</h4>
-            <div class="row">
+            <div class="row mt-4 mt-lg-0">
               <a id="btn-lg-default" href="#" target="blank" class="col-12">
                 <button>Registro elettronico</button>
               </a>
@@ -53,9 +53,9 @@ get_header();
           </frame>
         </div><!--#sidebar-presentazione -->
         
-        <div id="button-navigazione" class="col-12"> 
+        <div id="button-navigazione" class="col-12 my-4"> 
         <h4 class="d-lg-none">Scopri di più</h4>
-          <div class="row">
+          <div class="row mt-4 mt-lg-0">
             <div class="col-6 col-md-3">
               <a id="btn-lg-default-outline" href="#" target="blank">
                   <button><p>DIDATTICA</p> <span>Corso ITE serale</span></button>
@@ -84,10 +84,11 @@ get_header();
     <!--LOOP NEWS  -->
     <section id="loop-news-home" class="container"> 
 
-      <div class="row">
-        <div class="col-7">
+      <div class="row mt-5 mt-lg-0">
+        <div class="col-lg-7 col-12">
           <h4>Ultime news </h4>
-          <div class="row">
+          <div class="row mt-3 mt-lg-0 justify-content-between
+">
           
         
             <?php
@@ -101,7 +102,7 @@ get_header();
             
               while ($loop -> have_posts()) : $loop -> the_post(); ?> 
 
-              <article class="col-4 card"> 
+              <article class="col-lg-4 col-7 card"> 
                 <a href="<?php the_permalink();?>">
                   <div class="card-bg">
                     <div class="card-img-top card-img"> <?php the_post_thumbnail("news-thumb");?> </div>
@@ -113,18 +114,20 @@ get_header();
                 </a>  
               </article> <!--.card -->
             <?php endwhile; ?>
-
           </div><!--.row -->
           
-          <a id="btn-lg-default-outline mx-0" href="#" target="blank" class="col-12">
+          <div class="col-12 pr-0 d-block d-lg-none">
+          <a id="btn-lg-default-outline" href="#" target="blank" class="col-12 p-0">
             <button>Vai alla sezione</button>
           </a>
+        </div>
+        
         </div><!--col-8-->
 
         <!--LOOP PROGETTI  -->
-        <div class="col-4 offset-1">
-          <h4 >Alcuni dei nostri progetti </h4>
-          <div class="row">
+        <div class="col-12 col-lg-4 offset-lg-1 mt-5 mt-lg-0" id="progetti-home">
+          <h4>Alcuni dei nostri progetti </h4>
+          <div class="row mt-3 mt-lg-0">
 
           <?php
               $loop = new WP_Query( array( 
@@ -139,12 +142,14 @@ get_header();
 
               <article class="col-12"> 
                 <div class="row">
-                <a href="<?php the_permalink();?>">
-                  <div class="col-4">
+                
+                <div class="col-lg-4 col-4 row-img">
+                  <a href="<?php the_permalink();?>">
                     <?php the_post_thumbnail("project-thumb");?> 
-                  </div>
-                </a>
-                  <div class="col-8">
+                  </a>
+                </div>
+               
+                  <div class="col-lg-8 col-7">
                   <a href="<?php the_permalink();?>">
                     <h5><?php the_title(); ?></h5>
                   </a>
@@ -156,42 +161,51 @@ get_header();
               
               </article><!--.col-12 -->
             <?php endwhile; ?>
-            
-            <div class="col-12">
-              <a id="btn-lg-default-outline" href="#" target="blank">
-                <button>Vedi tutte</button>
-              </a>
-            </div><!--.col-12 -->
           </div><!--.row -->
-
         </div><!--col-3 offset-1 -->
       </div> <!--.row -->
+      
+      <!-- BUTTONS -->
+      <div class="row">
+        <div class="col-lg-7 p-0 d-none d-lg-block">
+          <a id="btn-lg-default-outline" href="#" target="blank" class="col-12 p-0">
+            <button>Vai alla sezione</button>
+          </a>
+        </div>
+        <div class="col-lg-4 col-12 offset-lg-1">
+          <div class="col-12 mt-4 mt-lg-0">
+            <a id="btn-lg-default-outline" href="#" target="blank">
+              <button>Vedi tutte</button>
+            </a>
+          </div><!--.col-12 -->
+        </div>
+      </div><!--.row -->
     </section><!--#loop-news-home .container -->
     
     <!--SPAZI E STORIA  -->
-    <section id="spazi-storia" class="container"> 
-      <div class="row rem5">
-        <div id="gli-spazi-img" class="col-6">
+    <section id="spazi-storia" class="container mt-5"> 
+      <div class="row mb-5">
+        <div id="gli-spazi-img" class="col-lg-6 col-12 mb-3 mb-lg-0">
           <img id="w100" src="https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-10.jpg?itok=Whi8hHo9" alt="">
         </div><!--#gli-spazi-img -->
 
-        <div id="gli-spazi-txt" class="col-6">
+        <div id="gli-spazi-txt" class="col-lg-6 col-12">
           <h4>Il nostro istituto</h4>
           <p>L’istituto Martino Martini è una scuola secondaria di secondo grado sita nel comune di Mezzolombardo, facilmente raggiungibile attraverso i mezzi di trasporto.
           Nell’istituto sono presenti 8 indirizzi di studio, 4 di ambito liceale e 4 di ambito tecnico.</p>
-          <a id="btn-lg-default" href="#" target="blank"><button class="wauto">Scopri</button></a>
+          <a id="btn-lg-default-outline" href="#" target="blank"><button class="wauto">Scopri</button></a>
         </div><!--#gli-spazi-txt -->
       </div><!--.row -->
       
       <div class="row">
-        <div id="storia-img" class="col-6 order-lg-2">
+        <div id="storia-img" class="col-lg-6 col-12 mb-3 mb-lg-0 order-lg-2">
           <img id ="w100"src="https://www.tandemconstruction.com/sites/default/files/styles/project_slider_main/public/images/project-images/IMG-Fieldhouse-10.jpg?itok=Whi8hHo9" alt="">
         </div><!--#storia-img --> 
-        <div id="storia-txt" class="col-6">
+        <div id="storia-txt" class="col-lg-6 col-12">
           <h4>La storia di Martino Martini</h4>
           <p>Missionario gesuita nato nel 1614 a Trento, Martino Martini fu un noto geografo e cartografo che visse a lungo nella Cina imperiale, viaggiando entro i suoi confini allo scopo di raccogliere informazioni di natura scientifica e geografica. 
           Il nostro istituto, che da sempre promuove diversi progetti nell’ambito dell’internazionalizzazione, prosegue idealmente la missione di Martino Martini riconoscendo il valore fondamentale per la nostra società della conoscenza approfondita del nuovo e del diverso e promuovendo la diffusione dei saperi tradizionali quale strumento ineludibile per la formazione di cittadini consapevoli e responsabili.</p>
-          <a id="btn-lg-default" href="#" target="blank"><button class="wauto">Scopri</button></a>
+          <a id="btn-lg-default-outline" href="#" target="blank"><button class="wauto">Scopri</button></a>
         </div><!--#storia-txt -->
       </div><!--.row -->
     </section><!--#spazi-storia .container -->
--- functions.php
+++ functions.php
@@ -145,7 +145,7 @@ if ( ! function_exists( 'dsi_setup' ) ) :
             add_image_size( 'item-thumb', 280, 280 , true);
             add_image_size( 'item-gallery', 730, 485 , true);
             add_image_size( 'vertical-card', 190, 290 , true);
-						add_image_size( 'news-thumb', auto, 150, true);
+						add_image_size( 'news-thumb', 150, 150, true);
 						add_image_size( 'project-thumb',110, 110, true);
             add_image_size( 'banner', 600, 250 , false);
         }
