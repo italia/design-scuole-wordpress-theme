dcdf5d1aed268116cb9f515ad84d3d2e6295f1f8
paolosartori <108754914+paolosartori@users.noreply.github.com>
2022-09-05 16:57:15 +0200

    Fix bullet list typography e modifiche hero.php

--- template-parts/hero/hero_page.php
+++ template-parts/hero/hero_page.php
@@ -1,9 +1,17 @@
+<?php 
+    $hero_post_id = "";
+?>
 <section>
     <div id="hero" class="container-fluid">
         <div id="content" class="row align-items-center">
             <div class="col-12">
                 <h1> <?php the_title(); ?> </h1>
-                <p class="h2">prova</p>
+                <p class="h3 fw-regular"><?php 
+                    get_the_title();
+                ?></p>
+                <a id="btn-lg-default" href="<?php 
+                    the_permalink($hero_post_id); 
+                ?>" target="blank" class="col-12"><button>Scopri</button></a>
             </div>
         </div>
     </div>
