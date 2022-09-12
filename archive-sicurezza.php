e9fa1a40cf713f9454f8866df51e144057e691f6
Eccel02 <stefano.eccel@artigianelli.tn.it>
2022-09-12 14:59:04 +0200

    archive-sicurezza

--- archive-sicurezza.php
+++ archive-sicurezza.php
@@ -1,31 +1,81 @@
 <?php
-/* 
-Template Name: Sicurezza
-*/
-get_header(); ?>
-<div id="primary" class="site-content">
-<div id="content" role="main">
- 
-<?php while ( have_posts() ) : the_post(); ?>
-                 
-<h1 class="entry-title"><?php the_title(); ?></h1>
- 
-<div class="entry-content">
- 
-<?php the_content(); ?>
- 
-/* Custom Archives Functions Go Below this line */
- 
- 
- 
-/* Custom Archives Functions Go Above this line */
- 
-</div><!-- .entry-content -->
- 
-<?php endwhile; // end of the loop. ?>
- 
-</div><!-- #content -->
-</div><!-- #primary -->
- 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Design_Scuole_Italia
+ */
+
+get_header();
+
+?>
+    <main id="main-container" class="main-container">
+        <?php get_template_part("template-parts/hero/hero_page"); ?>
+        <?php get_template_part("template-parts/common/breadcrumb"); ?>
+        <section id="container-spazi">
+            <div class="row">
+                <?php 
+
+                    $sicurezza = get_term_by("id", "file_sicurezza");
+                    $covid = get_term_by("id", "file_covid"); 
+
+                    $loop = new WP_Query( array(
+                        'post_type'         => 'sicurezza',
+                        'post_status'       => 'publish',
+                        'orderby'           => 'count',
+                        'order'             => 'DESC',
+                        'posts_per_page'    => 999 ,
+                    )); 
+                    
+                    while ($loop -> have_posts()) : $loop -> the_post( $sicurezza ); ?>
+
+                        <article>
+                            <div class="row">
+                                <div class="col-2"><i class="bi bi-filetype-pdf"></i></div>
+                                <div class="col-8"> 
+                                    <a href="<?php the_permalink();?>"> 
+                                        <?php the_title(); ?> 
+                                    </a> 
+                                </div>
+                            </div>        
+                        </article>
+
+                <?php endwhile; ?>
+
+                <!-- Secondo loop per il covid -->
+
+                <?php 
+
+                    $sicurezza = get_term_by("id", "file_sicurezza");
+                    $covid = get_term_by("id", "file_covid"); 
+
+                    $loop = new WP_Query( array(
+                        'post_type'         => 'sicurezza',
+                        'post_status'       => 'publish',
+                        'orderby'           => 'count',
+                        'order'             => 'DESC',
+                        'posts_per_page'    => 999 ,
+                    )); 
+
+                    while ($loop -> have_posts()) : $loop -> the_post( $covid ); ?>
+
+                        <article>
+                            <div class="row">
+                                <div class="col-2">icona</div>
+                                <div class="col-8"> 
+                                    <a href="<?php the_permalink();?>"> 
+                                        <?php the_title(); ?> 
+                                    </a> 
+                                </div>
+                            </div>        
+                        </article>
+
+                <?php endwhile; ?>
+
+            </div>
+            </section>
+        </main>
+
+    <?php
+    get_footer();
