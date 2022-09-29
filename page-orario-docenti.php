bee4db012364d715c8dc3499186d9933e5071997
Max Morozov <max@morozov.page>
2022-09-29 12:40:14 +0200

    Add highlight while searching `orario docenti`

--- page-orario-docenti.php
+++ page-orario-docenti.php
@@ -21,7 +21,13 @@
 
         <div class="col-6 mt-4 min-h">
 
-          <input type="text" id="myInput" onkeyup="filtro_Docenti()" placeholder="Cerca orari docenti">
+          <input 
+            type="search" 
+            id="myInput" 
+            onkeyup="filtro_Docenti(!true)" 
+            placeholder="Cerca orari docenti"
+            autocomplete="off"
+          >
 
           <ul id="myUL">
             <?php
@@ -80,20 +86,28 @@
 
   </section><!-- container -->
   <script>
-    function filtro_Docenti() {
+    function filtro_Docenti({doHighlight}) {
       // Declare variables
-      var input, filter, ul, li, a, i, txtValue;
+      var input, filter, ul, li, a, i, re, txtValue;
       input = document.getElementById('myInput');
       filter = input.value.toUpperCase();
       ul = document.getElementById("myUL");
       li = ul.getElementsByTagName('li');
+      re = new RegExp(filter, 'gi');
 
+      let text = filter;
       // Loop through all list items, and hide those who don't match the search query
       for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
           li[i].style.display = "";
+          // if (!doHighlight) continue;
+          text = a.innerHTML.replace(/(<mark class="highlight">|<\/mark>)/gim, '')
+          a.innerHTML = filter.length 
+            ? text.replace(re, '<mark class="highlight">$&</mark>')
+            : text;
+    
         } else {
           li[i].style.display = "none";
         }
