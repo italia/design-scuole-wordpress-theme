2c022ddbbfba5ca85ae0be026aaaa3ab22b58b04
marcelloscanavino <90757415+marcelloscanavino@users.noreply.github.com>
2022-10-11 14:50:18 +0200

    fix: authors link

--- template-parts/autore/card.php
+++ template-parts/autore/card.php
@@ -5,6 +5,10 @@ global $autore;
     <img src="<?php echo dsi_get_user_avatar($autore); ?>" alt="<?php echo esc_attr(dsi_get_display_name( $autore->ID )); ?>">
 </div><!-- /card-avatar-img -->
 <div class="card-avatar-content">
-    <p class="font-weight-normal">da <strong class="text-underline"><u><?php echo dsi_get_display_name( $autore->ID ); ?></u></strong></p>
+    <p class="font-weight-normal">da 
+        <a href="<?php echo get_author_posts_url( $autore->ID); ?>" aria-label="vai alla pagina di <?php echo dsi_get_display_name( $autore->ID ); ?>">
+            <strong class="text-underline"><u><?php echo dsi_get_display_name( $autore->ID ); ?></u></strong>
+        </a>
+    </p>
 	<small><?php echo dsi_get_user_role($autore); ?></small>
 </div><!-- /card-avatar-content -->
