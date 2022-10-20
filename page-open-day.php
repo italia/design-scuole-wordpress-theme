927b5f2371756d10518bba4255bd4cadcfd2cd1d
Alessandro Magnaguagno <alessandro.magnaguagno@artigianelli.tn.it>
2022-10-20 10:58:45 +0200

    impostazione open day

---
+++ page-open-day.php
@@ -0,0 +1,57 @@
+<?php
+/* Template Name: Open day
+ *
+ * didattica template file
+ *
+ * @package Design_Scuole_Italia
+ */
+
+get_header();
+
+
+?>
+
+<main id="main-container" class="main-container">
+       
+      
+       <?php get_template_part("template-parts/hero/hero_martini/hero_page"); ?> 
+
+
+    <section id="image-block" class="section bg-white">
+        <div class="container">
+            
+                
+                <div class="main-content col-12 col-lg-9">
+                    <div class="row variable-gutters mb-3">
+                        <div class="px-3">    
+                            <h4>Vuoi conoscere meglio la nostra scuola? Unisciti a noi e scopri i nostri Open Day dedicati ad ogni indirizzo di studio</h4>
+                            <p>L’Istituto apre le porte alle famiglie per guidarvi nella scelta della Scuola superiore. <br>
+                            Partecipando potrete vedere i locali, parlare con gli insegnanti e con i ragazzi, assistere a una presentazione della proposta formativa della scuola e essere guidati in alcune piccole esperienze di laboratorio.
+                            </p>
+                        </div>
+                    </div><!-- row -->
+                    <div class="row variable-gutters mb-3">
+                        <div class="px-3">
+                            
+
+                        </div>
+
+                    </div>
+
+
+                </div><!-- content -->
+                
+
+
+            
+        </div><!-- /container -->
+    </section>
+
+
+
+</main>
+
+
+
+<?php
+get_footer();
