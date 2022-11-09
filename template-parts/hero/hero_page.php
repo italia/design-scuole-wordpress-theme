fd25c2e7da53ed900044738502cb7dc0b527124a
paolosartori <108754914+paolosartori@users.noreply.github.com>
2022-11-09 09:13:24 +0100

    fix hero slider

--- template-parts/hero/hero_page.php
+++ template-parts/hero/hero_page.php
@@ -1,30 +1,30 @@
-<?php 
-    $hero_post_id = 134;
+<?php
+$hero_post_id = 134;
 ?>
 <section class="hero-container">
-    <div id="hero-splide" >
+    <div id="hero-splide">
         <div id="hero-content" class="row align-items-center splide__track">
             <ul class="splide__list ">
 
-                <?php 
+                <?php
                 // TODO: Change loop in `hero` slider
                 // labels: enhancement, help wanted
                 // assignees: maxmorozoff, ToldoDesign128, paolosartori
                 ?>
-                <?php for ($i=0; $i < 5; $i++) { ?>
+                <?php for ($i = 0; $i < 5; $i++) { ?>
 
-                <li class="splide__slide">
-                    <h1> <?php the_title(); ?> </h1>
-                    <p class="h3 fw-regular"><?php 
-                        echo get_the_title($hero_post_id);
-                    ?></p>
-                    <div class="text-center ">
-                        <a class="btn-md-default" class="" href="<?php 
-                        echo get_the_permalink($hero_post_id); 
-                        ?>" target="blank" class="col-12"><button class="w-auto">Scopri</button></a>
-                    </div>
-                    <div class="splide__slide__img" style="background-image: url( https://source.unsplash.com/random?<?php echo $i; ?> );"></div>
-                </li>
+                    <li class="splide__slide">
+                        <h1> <?php the_title(); ?> </h1>
+                        <p class="h3 fw-regular"><?php
+                                                    echo get_the_title($hero_post_id);
+                                                    ?></p>
+                        <div class="text-center ">
+                            <a class="btn-md-default" class="" href="<?php
+                                                                        echo get_the_permalink($hero_post_id);
+                                                                        ?>" target="blank" class="col-12"><button class="w-auto">Scopri</button></a>
+                        </div>
+                        <div class="splide__slide__img" style="background-image: url( https://source.unsplash.com/random?<?php echo $i; ?> );"></div>
+                    </li>
 
                 <?php } ?>
             </ul>
@@ -35,8 +35,8 @@
     /**
      *  @see {@link https://splidejs.com/v3/guides/getting-started/ } 
      */
-    document.addEventListener( 'DOMContentLoaded', () =>
-        (!!Splide) && (new Splide( '#hero-splide', {
+    document.addEventListener('DOMContentLoaded', () =>
+        (!!Splide) && (new Splide('#hero-splide', {
             type: 'loop',
             pagination: true,
             start: 0,
@@ -46,29 +46,3 @@
             },
         })).mount() && (document.querySelector('#hero-splide .splide__list').style = ''));
 </script>
-<!-- <section id="image-carousel" class="splide hero container-fluid" aria-label="Beautiful Images">
-  <div class="splide__track">
-		<ul class="splide__list">
-            <?php 
-            // $loop = new WP_Query( array(
-            //     'post_type'         => 'slider',
-            //     'post_status'       => 'publish',
-            //     'orderby'           => 'count',
-            //     'order'             => 'DESC',
-            //     'posts_per_page'    => 999 ,
-            // ));
-            // while ($loop -> have_posts()) : $loop -> the_post();
-             
-            // ?>
-
-			// <li class="splide__slide">
-            //     <h1><?php the_title(); ?></h1>   
-			// 	<?php the_post_thumbnail();?>
-			// </li>
-            
-            // <?php
-                endwhile;
-            ?>
-		</ul>
-  </div>
-</section> -->
