f7b13075dd3d79e7324d5faca764f58cbb45c137
Federico <fede.toldo96@gmail.com>
2022-11-13 18:07:30 +0100

    fix cic

---
+++ functions-parts/back-end-custom.php
@@ -0,0 +1,68 @@
+<?php
+    /*WP custom backend
+    --------------------------------*/
+    
+    //custom wp backend back = 17x17px
+    function WWS_custom_back() {
+        echo '<style type="text/css">
+            #wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before {
+                background-image: url(' . get_bloginfo('stylesheet_directory') . 'assets/images_martini/logo-custom.png) !important;
+                background-position: 0 0;
+                background-size: cover;
+                color:rgba(0, 0, 0, 0);
+            }
+             #wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon {
+                background-position: 0 0;
+            }
+            .wp-admin div#ays-quiz-winter-dicount-main div#ays-quiz-dicount-month-main.ays_quiz_dicount_info{display:none}
+            /*hide dashboard*/
+            .wp-admin #adminmenuwrap #menu-dashboard {display:none !important;}
+            /*hide pages*/
+            .wp-admin #adminmenuwrap #menu-pages {display:none !important;}
+            /*hide customizer*/
+            .wp-admin #adminmenuwrap #adminmenu .wp-submenu li.hide-if-no-customize {display:none !important;}
+            /*hide theme editor*/
+            .wp-admin #adminmenuwrap #adminmenu .wp-submenu li a[href="theme-editor.php"] {display:none !important;}
+            /*hide plugin*/
+            .wp-admin #adminmenuwrap #menu-plugins {display:none !important;}
+            .wp-admin #wpadminbar #wp-toolbar #wp-admin-bar-updates {display:none !important;}
+            
+            /*hide all in one*/
+            .wp-admin #adminmenuwrap .toplevel_page_ai1wm_export {display:none !important;}
+            /*hide writing settings*/
+            .wp-admin #adminmenuwrap #menu-settings ul li:nth-of-type(3) {display:none !important;}
+
+			#menu-posts-struttura {display:none !important;}
+
+			#toplevel_page_dsi_options {display:none !important;}
+			
+			#menu-posts-luogo {display:none !important;}
+			
+			#menu-posts-servizio {display:none !important;}
+			
+			#menu-posts-evento {display:none !important;}
+			
+			#menu-comments {display:none !important;}
+			
+			#menu-posts-avcp {display:none !important;}
+
+			#menu-posts-spesa {display:none !important;}
+
+			#menu-posts-incarico {display:none !important;}
+
+			#toplevel_page_impostazioni-wpgov {display:none !important;}
+
+			#menu-appearance {display:none !important;}
+
+			#menu-plugins {display:none !important;}
+
+			#menu-users {display:none !important;}
+
+			#menu-tools {display:none !important;}
+
+			#menu-settings {display:none !important;}
+
+			#toplevel_page_cfdb7-list {display:none !important;}
+        </style>';
+    }
+    add_action('wp_before_admin_bar_render', 'WWS_custom_back');
