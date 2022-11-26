0ce3bdf0b77c3b9299a58e3625bc59712388ed7c
Odraode04 <33807145+Odraode04@users.noreply.github.com>
2022-11-26 19:13:51 +0200

    fix: single-evento foto e video

--- single-evento.php
+++ single-evento.php
@@ -419,26 +419,26 @@ $user_can_view_post = dsi_members_can_user_view_post(get_current_user_id(), $pos
                 </div><!-- /container -->
             </section>
 
-            <section class="section bg-gray-light py-5" id="art-par-04">
-                <div class="container py-4">
-                    <div class="title-section text-center mb-5">
-                        <h2 class="h4">Foto e video</h2>
-                    </div><!-- /title-large -->
-                    <div class="row variable-gutters">
-                        <div class="col">
-                        <?php if ( is_array( $gallery ) && count( $gallery ) > 0 ) { ?>
-                            <div class="it-carousel-wrapper simple-two-carousel splide" data-bs-carousel-splide>
-                                <div class="splide__track">
-                                    <ul class="splide__list">
-                                    <?php get_template_part( "template-parts/single/gallery", $post->post_type ); ?>
-                                    </ul>
-                                </div><!-- /carousel-simple -->
-                            </div>
-                        <?php } ?>
-                        </div><!-- /col -->
-                    </div><!-- /row -->
-                </div><!-- /container -->
-            </section>
+            <?php if ( is_array( $gallery ) && count( $gallery ) > 0 ) { ?>
+                <section class="section bg-gray-light py-5" id="art-par-04">
+                    <div class="container py-4">
+                        <div class="title-section text-center mb-5">
+                            <h2 class="h4">Foto e video</h2>
+                        </div><!-- /title-large -->
+                        <div class="row variable-gutters">
+                            <div class="col">
+                                <div class="it-carousel-wrapper simple-two-carousel splide" data-bs-carousel-splide>
+                                    <div class="splide__track">
+                                        <ul class="splide__list">
+                                        <?php get_template_part( "template-parts/single/gallery", $post->post_type ); ?>
+                                        </ul>
+                                    </div><!-- /carousel-simple -->
+                                </div>
+                            </div><!-- /col -->
+                        </div><!-- /row -->
+                    </div><!-- /container -->
+                </section>
+            <?php } ?>
 
 			<?php get_template_part("template-parts/single/more-posts"); ?>
 
