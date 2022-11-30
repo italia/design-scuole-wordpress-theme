a8cd961c6092a10975da5cafe564fd2d308bc80a
Francesco Improta <francesco@designabile.com>
2022-11-30 10:25:12 +0100

    fix: renamed secondary nav items

--- inc/activation.php
+++ inc/activation.php
@@ -651,7 +651,7 @@ function dsi_create_pages_on_theme_activation() {
 
         $term = get_term_by("name", "Personale Scolastico", "tipologia-servizio");
         wp_update_nav_menu_item($menu->term_id, 0, array(
-            'menu-item-title' => __('Servizi per il personale scolastico', "design_scuole_italia"),
+            'menu-item-title' => __('Personale scolastico', "design_scuole_italia"),
             'menu-item-status' => 'publish',
             'menu-item-type' => 'taxonomy',
             'menu-item-object' => 'tipologia-servizio',
@@ -662,7 +662,7 @@ function dsi_create_pages_on_theme_activation() {
 
         $term = get_term_by("name", "Famiglie e Studenti", "tipologia-servizio");
         wp_update_nav_menu_item($menu->term_id, 0, array(
-            'menu-item-title' => __('Servizi per famiglie e studenti', "design_scuole_italia"),
+            'menu-item-title' => __('Famiglie e studenti', "design_scuole_italia"),
             'menu-item-status' => 'publish',
             'menu-item-type' => 'taxonomy',
             'menu-item-object' => 'tipologia-servizio',
