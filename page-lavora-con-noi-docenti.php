95a00886795a66181433d0149eb4f46fb1d8528e
paolosartori <108754914+paolosartori@users.noreply.github.com>
2022-12-01 10:52:51 +0100

    lavora con noi

--- page-lavora-con-noi-docenti.php
+++ page-lavora-con-noi-docenti.php
@@ -13,7 +13,7 @@ get_header()
 <main id="main-container" class="main-container">
     <?php get_template_part("martini-template-parts/hero/hero_title") ?>
 
-    <section id="input-lavora-con-noi-docenti" class="container mt-5">
+    <section id="input-lavora-con-noi-docenti" class="container mt-5 mb-5">
         <div class="row">
             <div class="col-12">
                 <?php echo do_shortcode('[contact-form-7 id="126" title="Lavora con noi - Docenti"]') ?>
