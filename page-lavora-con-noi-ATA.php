8e182a3742790b4673315fb4ef204057322acbb7
paolosartori <108754914+paolosartori@users.noreply.github.com>
2022-12-05 12:41:39 +0100

    page style lavora con noi

--- page-lavora-con-noi-ATA.php
+++ page-lavora-con-noi-ATA.php
@@ -4,7 +4,7 @@ get_header()
 <main id="main-container" class="main-container">
     <?php get_template_part("martini-template-parts/hero/hero_title") ?>
 
-    <section id="input-lavora-con-noi-docenti" class="container mt-5 mb-5">
+    <section id="input-lavora-con-noi-ata" class="container mt-5 mb-5">
         <div class="row">
             <div class="col-12">
                 <?php echo do_shortcode('[contact-form-7 id="743" title="Lavora con noi - ATA"]') ?>
