a863d1054e54bf2ff9294700acd4ba17742f9a83
Francesco Barile <48310974+barilefrancesco@users.noreply.github.com>
2022-12-15 11:42:44 +0100

    Problema accessibilità header
    Ho aggiunto "aria-label" al botton Accedi. Questo risolve il problema agli screen reader.
    Testando il sito con "app-valutazione-scuole" porta la voce "Test aggiuntivi" da 89 a 93.

--- template-parts/header/header-anon.php
+++ template-parts/header/header-anon.php
@@ -1,4 +1,4 @@
-<button type="button" class="icon-text" data-toggle="modal" data-target="#access-modal">
+<button type="button" class="icon-text" aria-label="<?php _e("Accedi", "design_scuole_italia"); ?>" data-toggle="modal" data-target="#access-modal">
     <span class="mr-2 d-none d-lg-block"><?php _e("Accedi", "design_scuole_italia"); ?></span>
     <div class="icon-wrapper">
         <svg class="svg-user"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-user"></use></svg>
