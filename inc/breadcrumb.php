a8d5dddbd0b05790f9dd0b37c80b2d4cc67c5137
Igor Škorjanc <igor.skorjanc@gmail.com>
2023-01-25 18:14:46 +0100

    Fix some Notice messages on php7.4

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -589,7 +589,7 @@ class Breadcrumb_Trail {
 		if ( false !== $taxonomy->rewrite ) {
 
 			// If 'with_front' is true, dd $wp_rewrite->front to the trail.
-			if ( $taxonomy->rewrite['with_front'] && $wp_rewrite->front )
+			if ( $taxonomy->rewrite['with_front']  ?? '' && $wp_rewrite->front )
 				$this->add_rewrite_front_items();
 
 			// Get parent pages by path if they exist.
@@ -1059,7 +1059,7 @@ class Breadcrumb_Trail {
 
 		foreach ( $post_types as $type ) {
 
-			if ( $slug === $type->has_archive || ( true === $type->has_archive && $slug === $type->rewrite['slug'] ) )
+			if ( $slug === $type->has_archive || ( true === $type->has_archive ?? '' && $slug === $type->rewrite['slug'] ) )
 				$return[] = $type;
 		}
 
