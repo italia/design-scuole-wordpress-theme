db5f834a31fbd6ec351f87529a93d9bf5becb232
Max Morozov <max@morozov.page>
2023-01-26 16:15:01 +0100

    update `scheda-progetto` page

--- archive-scheda_progetto.php
+++ archive-scheda_progetto.php
@@ -17,48 +17,43 @@ if(is_post_type_archive("scheda_didattica")){
 get_header();
 ?>
 
-    <main id="main-container" class="main-container <?php echo $class; ?>">
-
-    <?php get_template_part("martini-template-parts/hero/hero_title"); ?>
-        <?php get_template_part("template-parts/search/filters-scheda-progetto"); ?>
-
-        <section class="section">
-            <div class="container mt-5">
-                <div class="variable-gutters">
-
-						<?php if ( have_posts() ) : ?> 
-                            
-                            <div class="row" id="container_archive">
-							<?php
-							/* Start the Loop */
-							while ( have_posts() ) :
-								the_post();
-								get_template_part( 'template-parts/list/article-scheda_progetto', get_post_type() );
-							endwhile;
-							?>
-                            </div>
-                            
-                            <nav class="pagination-wrapper" aria-label="Navigazione della pagina">
-								<?php echo dsi_bootstrap_pagination(); ?>
-                            </nav>
-						<?php
-						else :
-
-							get_template_part( 'template-parts/content', 'none' );
-
-						endif;
-						?>
-
-                        <?php if(isset($_GET["archive"]) && ($_GET["archive"] == "true")){ ?>
-                            <p><a class="btn btn-block btn-secondary d-none" href="<?php echo get_post_type_archive_link("scheda_progetto"); ?>" ><?php _e("Consulta i progetti dell'anno in corso", "design_scuole_italia"); ?></a></p>
-                        <?php }else{ ?>
-                            <p><a class="btn btn-block btn-secondary d-none" href="<?php echo get_post_type_archive_link("scheda_progetto"); ?>?archive=true" ><?php _e("Consulta i progetti degli scorsi anni", "design_scuole_italia"); ?></a></p>
-                        <?php } ?>
-                    
-                </div><!-- /row -->
-            </div><!-- /container -->
-        </section>
-    </main>
+<main id="main-container" class="main-container <?php echo $class; ?>">
+<?php get_template_part("martini-template-parts/hero/hero_title"); ?>
+    <?php get_template_part("template-parts/search/filters-scheda-progetto"); ?>
+    <section class="section">
+        <div class="container mt-5">
+            <div class="variable-gutters">
+                <?php if ( have_posts() ) { ?> 
+                <div class="row" id="container_archive">
+                    <?php
+                        while ( have_posts() ) {
+                            the_post();
+                    ?> 
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <?php 
+                            // TODO change card template
+                            get_template_part( 'martini-template-parts/card', get_post_type() );
+                        ?> 
+                    </div>
+                    <?php }; ?>
+                </div>
+                <nav class="pagination-wrapper" aria-label="Navigazione della pagina">
+                    <?php echo dsi_bootstrap_pagination(); ?>
+                </nav>
+                <?php
+                    } else {
+                        get_template_part( 'template-parts/content', 'none' );
+                    };
+                ?>
+                <?php if(isset($_GET["archive"]) && ($_GET["archive"] == "true")){ ?>
+                    <p><a class="btn btn-block btn-secondary d-none" href="<?php echo get_post_type_archive_link("scheda_progetto"); ?>" ><?php _e("Consulta i progetti dell'anno in corso", "design_scuole_italia"); ?></a></p>
+                <?php }else{ ?>
+                    <p><a class="btn btn-block btn-secondary d-none" href="<?php echo get_post_type_archive_link("scheda_progetto"); ?>?archive=true" ><?php _e("Consulta i progetti degli scorsi anni", "design_scuole_italia"); ?></a></p>
+                <?php } ?>
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </section>
+</main>
 
 <?php
 get_footer();
--- martini-template-parts/loop/loop-progetti-generale.php
+++ martini-template-parts/loop/loop-progetti-generale.php
@@ -20,48 +20,14 @@
 
           while ($loop -> have_posts()) : $loop -> the_post(); 
 
+          ?> 
+
+          <div class="col-12 col-lg-4">
+              <?php get_template_part( 'martini-template-parts/card', get_post_type() );?> 
+          </div>
+      <?php
         ?>
-        <div class="col-12 col-lg-4">
         
-          <article class="card">
-
-            <div class="card-img-top card-img position-relative">
-
-              <a class="img-loop" href="<?php the_permalink();?>">
-
-                <?php 
-                if ( (function_exists('has_post_thumbnail')) && (has_post_thumbnail()) ) {
-                  echo get_the_post_thumbnail($post->ID);
-                }
-              ?>
-                <div>
-                </div>
-
-              </a>
-
-              <div class="position-absolute" id="card_article_badge"> 
-
-                <?php get_template_part("template-parts/common/badges-argomenti"); ?>
-
-              </div>
-
-            </div>
-
-            <div class="card-body">
-
-              <p class="card-title primary"> <?php the_title(); ?> </p>
-
-              <a href="<?php the_permalink();?>" class="btn-mini-default"> 
-
-                <button class="w-auto">Scopri</button>
-
-              </a>
-
-            </div>
-
-          </article> <!--.card -->
-
-        </div>
 
           <?php endwhile; ?>
 
