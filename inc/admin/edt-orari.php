769d02e0856ec53ff27e1c56214ac7ba5c8f7234
Max Morozov <max@morozov.page>
2023-01-29 17:43:08 +0100

    add webhook and do cleanup

--- inc/admin/edt-orari.php
+++ inc/admin/edt-orari.php
@@ -1,64 +1,5 @@
 <?php
 
-
-// Register Custom Post Type
-// function orari_post_type() {
-
-// 	$labels = array(
-// 		'name'                  => _x( 'orari', 'Post Type General Name', 'text_domain' ),
-// 		'singular_name'         => _x( 'orario', 'Post Type Singular Name', 'text_domain' ),
-// 		'menu_name'             => __( 'Orari', 'text_domain' ),
-// 		'name_admin_bar'        => __( 'Orari Admin Bar Name', 'text_domain' ),
-// 		'archives'              => __( 'Item Archives', 'text_domain' ),
-// 		'attributes'            => __( 'Item Attributes', 'text_domain' ),
-// 		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-// 		'all_items'             => __( 'All Items', 'text_domain' ),
-// 		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
-// 		'add_new'               => __( 'Add New', 'text_domain' ),
-// 		'new_item'              => __( 'New Item', 'text_domain' ),
-// 		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-// 		'update_item'           => __( 'Update Item', 'text_domain' ),
-// 		'view_item'             => __( 'View Item', 'text_domain' ),
-// 		'view_items'            => __( 'View Items', 'text_domain' ),
-// 		'search_items'          => __( 'Search Item', 'text_domain' ),
-// 		'not_found'             => __( 'Not found', 'text_domain' ),
-// 		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-// 		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-// 		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-// 		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-// 		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-// 		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-// 		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-// 		'items_list'            => __( 'Items list', 'text_domain' ),
-// 		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-// 		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
-// 	);
-// 	$args = array(
-// 		'label'                 => __( 'orario', 'text_domain' ),
-// 		'description'           => __( 'Post Type Description', 'text_domain' ),
-// 		'labels'                => $labels,
-// 		'supports'              => array( 'title', 'editor', 'custom-fields', 'page-attributes', 'post-formats' ),
-// 		'taxonomies'            => array( 'orari_taxonomies' ),
-// 		'hierarchical'          => false,
-// 		'public'                => true,
-// 		'show_ui'               => true,
-// 		'show_in_menu'          => true,
-// 		'menu_position'         => 5,
-// 		'menu_icon'             => 'dashicons-clock',
-// 		'show_in_admin_bar'     => true,
-// 		'show_in_nav_menus'     => true,
-// 		'can_export'            => true,
-// 		'has_archive'           => true,
-// 		'exclude_from_search'   => true,
-// 		'publicly_queryable'    => true,
-// 		'capability_type'       => 'post',
-// 		'show_in_rest'          => true,
-// 	);
-// 	register_post_type( 'orari', $args );
-
-// }
-// add_action( 'init', 'orari_post_type', 0 );
-
 // Register custom post type 'orari'
 function orari_post_type()
 {
@@ -131,51 +72,67 @@ function orari_taxonomies()
 }
 add_action('init', 'orari_taxonomies', 0);
 
-
-// Create custom "Orari" tab in WordPress dashboard
-// add_action('admin_menu', 'orari_tab');
-// function orari_tab()
-// {
-//     add_menu_page(
-//         'Orari Tab',
-//         'Orari',
-//         'manage_options',
-//         'orari',
-//         'orari_settings_page',
-//         'dashicons-clock',
-//         6
-//     );
-// }
-// Remove the 'Orari Categories' submenu page
-function remove_orari_categories_submenu() {
-    // global $submenu;
-    //  console_log(json_encode($submenu)); //this will print out all the menus/submenus currently available in the admin.
-    // remove_submenu_page( 'orari', 'edit-tags.php?taxonomy=orari_category' );
-    remove_submenu_page( 'edit.php?post_type=orari', 'post-new.php?post_type=orari' );
-    
-}
-add_action( 'admin_menu', 'remove_orari_categories_submenu', 11 );
-
-// // Add the categories management page and the custom post type page as submenus of the main 'orari' tab
-function add_orari_submenus()
+// Add custom field "source URL" to orari taxonomy
+function orari_taxonomy_custom_fields_edit($tag)
 {
-    // add_submenu_page(
-    //     // 'orari',
-    //     'edit.php?post_type=orari',
-    //     'All Orari',
-    //     'All Orari',
-    //     'manage_options',
-    //     'edit.php?post_type=orari'
-    // );
-    // add_submenu_page(
-    //     'orari',
-    //     'Orari Categories',
-    //     'Categories',
-    //     'manage_options',
-    //     'edit-tags.php?taxonomy=orari_category'
-    // );
+    $t_id = $tag->term_id;
+    $term_meta = get_option("taxonomy_term_$t_id");
+?>
+    <tr class="form-field">
+        <th scope="row" valign="top">
+            <label for="term_meta[source_url]"><?php _e('Source URL'); ?></label>
+        </th>
+        <td>
+            <input type="text" name="term_meta[source_url]" id="term_meta[source_url]" value="<?php echo esc_attr($term_meta['source_url']) ? esc_attr($term_meta['source_url']) : ''; ?>">
+            <p class="description"><?php _e('Enter the source URL for this category'); ?></p>
+        </td>
+    </tr>
+<?php
 }
-add_action('admin_menu', 'add_orari_submenus', 11);
+add_action('orari_category_edit_form_fields', 'orari_taxonomy_custom_fields_edit', 10, 1);
+
+// Add custom field "source URL" to orari taxonomy
+function orari_taxonomy_custom_fields_add($tag)
+{
+    $t_id = $tag->term_id;
+    $term_meta = get_option("taxonomy_term_$t_id");
+?>
+
+    <div class="form-field">
+        <label for="term_meta[source_url]"><?php _e('Source URL'); ?></label>
+
+        <input type="text" name="term_meta[source_url]" id="term_meta[source_url]" value="<?php echo esc_attr($term_meta['source_url']) ? esc_attr($term_meta['source_url']) : ''; ?>">
+        <p class="description"><?php _e('Enter the source URL for this category'); ?></p>
+    </div>
+<?php
+}
+add_action('orari_category_add_form_fields', 'orari_taxonomy_custom_fields_add', 10, 1);
+
+// Save custom field "source URL" value
+function save_orari_taxonomy_custom_fields($term_id)
+{
+    if (isset($_POST['term_meta'])) {
+        $tax_term_id = 'taxonomy_term_' . $term_id;
+        $term_meta = get_option($tax_term_id);
+        $cat_keys = array_keys($_POST['term_meta']);
+        foreach ($cat_keys as $key) {
+            if (isset($_POST['term_meta'][$key])) {
+                $term_meta[$key] = $_POST['term_meta'][$key];
+            }
+        }
+        update_option($tax_term_id, $term_meta);
+    }
+}
+add_action('edited_orari_category', 'save_orari_taxonomy_custom_fields', 10, 2);
+
+
+// Remove the 'Orari Add new' submenu page
+function remove_orari_add_submenu()
+{
+    remove_submenu_page('edit.php?post_type=orari', 'post-new.php?post_type=orari');
+    remove_submenu_page('edit-tags.php?post_type=orari', 'edit-tags.php?taxonomy=orari_category&post_type=orari');
+}
+add_action('admin_menu', 'remove_orari_add_submenu', 11);
 
 function orari_settings_page()
 {
@@ -184,7 +141,6 @@ function orari_settings_page()
     <div class="wrap">
         <h1>Orari Settings</h1>
         <form method="post" action="options.php">
-            <h2>Orari Settings</h2>
             <?php
             settings_fields('orari_settings');
             do_settings_sections('orari');
@@ -194,67 +150,49 @@ function orari_settings_page()
     </div>
 <?php
 }
+
 // Register submenu for custom categories of 'orari'
 function orari_submenu()
 {
-    // add_options_page(
-    //     'Orari Settings',
-    //     'Orari Settings',
-    //     'manage_options',
-    //     'orari-settings',
-    //     'orari_settings_page',
-    //     // 6
-    // );
     add_submenu_page(
+        /* parent_slug */
         'edit.php?post_type=orari',
+        /* page_title  */
         'Orari Settings',
+        /* menu_title  */
         'Orari Settings',
+        /* capability  */
         'manage_options',
+        /* menu_slug   */
         'orari-settings',
+        /* callback    */
         'orari_settings_page',
+        /* position    */
         0
     );
 
     $taxonomy = 'orari_category';
     $categories = get_terms($taxonomy);
     if (!empty($categories)) {
-        // add_submenu_page(
-        //     'orari',
-        //     'Orari Categories',
-        //     'Orari Categories',
-        //     'manage_options',
-        //     // 'edit-tags.php?taxonomy=' . $taxonomy
-        //     'edit-tags.php?taxonomy=' . $taxonomy . '&post_type=orari'
-        // );
         foreach ($categories as $category) {
             add_submenu_page(
-                // 'orari',
+                /* parent_slug */
                 'edit.php?post_type=orari',
+                /* page_title  */
                 $category->name,
+                /* menu_title  */
                 $category->name,
+                /* capability  */
                 'manage_options',
+                /* menu_slug   */
                 'edit.php?post_type=orari&orari_category=' . $category->slug
+                /* callback    */
+                /* position    */
             );
         }
     }
 }
 add_action('admin_menu', 'orari_submenu');
-
-// Create custom "Orari" tab in WordPress dashboard
-// add_action('admin_menu', 'orari_tab');
-// function orari_tab()
-// {
-//     add_menu_page(
-//         'Orari Tab',
-//         'Orari',
-//         'manage_options',
-//         'orari',
-//         'orari_settings_page',
-//         'dashicons-clock',
-//         6
-//     );
-// }
-
 
 // Register settings and fields for the Orari tab
 add_action('admin_init', 'orari_settings');
@@ -263,12 +201,23 @@ function orari_settings()
     register_setting('orari_settings', 'orari_source_url');
     register_setting('orari_settings', 'orari_update_interval');
     register_setting('orari_settings', 'orari_upload_url');
+    register_setting('orari_settings', 'orari_webhook_uuid');
 
     add_settings_section('orari_section', '', 'orari_section_callback', 'orari');
 
     add_settings_field('orari_source_url', 'Source URL', 'orari_source_url_callback', 'orari', 'orari_section');
     add_settings_field('orari_update_interval', 'Update Interval', 'orari_update_interval_callback', 'orari', 'orari_section');
     add_settings_field('orari_upload_url', 'Upload to URL', 'orari_upload_url_callback', 'orari', 'orari_section');
+    add_settings_field('orari_webhook_uuid', 'webhook uuid', 'orari_webhook_url_callback', 'orari', 'orari_section');
+}
+
+function get_orari_option($option, $default = false)
+{
+    $orari_settings = get_option('orari_settings', $default);
+    if (isset($orari_settings[$option]) || !empty($orari_settings[$option])) {
+        return $orari_settings[$option];
+    }
+    return $default;
 }
 
 function orari_section_callback()
@@ -279,21 +228,52 @@ function orari_section_callback()
 
 function orari_source_url_callback()
 {
-    $source_url = esc_attr(get_option('orari_source_url'));
+    $source_url = esc_attr(get_orari_option('orari_source_url'));
     echo '<input type="text" name="orari_source_url" value="' . $source_url . '" placeholder="https://example.com/data.json" />';
 }
 
 function orari_update_interval_callback()
 {
-    $update_interval = esc_attr(get_option('orari_update_interval'));
+    $update_interval = esc_attr(get_orari_option('orari_update_interval'));
     echo '<input type="text" name="orari_update_interval" value="' . $update_interval . '" placeholder="60" />';
 }
 
 function orari_upload_url_callback()
 {
-    $upload_url = esc_attr(get_option('orari_upload_url'));
+    $upload_url = esc_attr(get_orari_option('orari_upload_url'));
     echo '<input type="text" name="orari_upload_url" value="' . $upload_url . '" placeholder="https://example.com/upload" />';
 }
+
+function orari_webhook_url_callback()
+{
+    $webhook_uuid = esc_attr(get_orari_option('orari_webhook_uuid'));
+    console_log($webhook_uuid);
+
+    echo '<input type="text" name="orari_webhook_uuid" value="' . $webhook_uuid . '" placeholder="https://example.com/upload" />';
+}
+
+// Generate and store UUID for the webhook
+function orari_generate_webhook_uuid()
+{
+    $orari_settings = get_option('orari_settings');
+    console_log($orari_settings);
+    if (!isset($orari_settings['orari_webhook_uuid']) || empty($orari_settings['orari_webhook_uuid'])) {
+        $orari_settings['orari_webhook_uuid'] = wp_generate_uuid4();
+        update_option('orari_settings', $orari_settings);
+    }
+}
+add_action('init', 'orari_generate_webhook_uuid');
+
+// Webhook URL to trigger orari_cron_job
+function orari_webhook_callback()
+{
+    $orari_settings = get_option('orari_settings');
+    if (isset($_GET['orari_webhook']) && $_GET['orari_webhook'] == $orari_settings['orari_webhook_uuid']) {
+        orari_cron_job();
+        exit;
+    }
+}
+add_action('init', 'orari_webhook_callback');
 
 // Function to create the Cron job
 function orari_cron_job()
