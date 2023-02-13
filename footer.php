99395417102dc75da3318557fe12e6cedd87daf0
Andrea Stagi <stagi.andrea@gmail.com>
2023-02-13 18:23:45 +0100

    fix(a11y): alt text for wu image

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
             <div class="col logos-wrapper">
                 <img class="ue-logo"
                     src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/logo-eu-inverted.svg' ); ?>"
-                    alt="logo Unione Europea"
+                    alt="Finanziato dall' Unione Europea - Next generation EU"
                 >
                 <div class="logo-footer">
                     <?php get_template_part("template-parts/common/logo"); ?>
