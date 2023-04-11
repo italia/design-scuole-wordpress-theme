57330067a5e8c430a91792245b75d5de98023d11
rcoopcsc <125898973+rcoopcsc@users.noreply.github.com>
2023-04-11 09:38:14 +0200

    Update card-circolare.php

--- template-parts/single/card-circolare.php
+++ template-parts/single/card-circolare.php
@@ -1,13 +1,3 @@
 <?php
-global $post;
-$numerazione_circolare = dsi_get_meta("numerazione_circolare", "", $post->ID);
 
-?><div class="card card-bg bg-white card-thumb-rounded">
-	<div class="card-body">
-		<div class="card-content">
-			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
-            <small class="h6 text-greendark"><?php _e("circ. n.", "design_scuole_italia"); echo $numerazione_circolare; ?></small>
-			<p><?php echo get_the_excerpt($post); ?></p>
-		</div>
-	</div><!-- /card-body -->
-</div><!-- /card --><?php
+?><?php
