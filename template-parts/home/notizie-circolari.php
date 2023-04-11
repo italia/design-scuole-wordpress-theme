9510acfaa9c27b4fe7aac24ad2053a731de4af8e
rcoopcsc <125898973+rcoopcsc@users.noreply.github.com>
2023-04-11 09:34:11 +0200

    Update notizie-circolari.php

--- template-parts/home/notizie-circolari.php
+++ template-parts/home/notizie-circolari.php
@@ -12,24 +12,7 @@ if($ct%2)
 		<div class="title-section mb-5">
 			<h2 class="h4"><?php _e("Circolari", "design_scuole_italia"); ?></h2>
 		</div><!-- /title-large -->
-		<div class="it-carousel-wrapper carousel-notice it-carousel-landscape-abstract-three-cols splide" data-bs-carousel-splide>
-			<div class="splide__track">
-				<ul class="splide__list">
-					<?php
-					$args = array('post_type' => 'circolare',
-								'posts_per_page' => 9,
-					);
-					$posts = get_posts($args);
-					foreach ($posts as $post){ ?>				
-					<li class="splide__slide">
-						<div class="it-single-slide-wrapper h-100">
-							<?php get_template_part("template-parts/single/card", "vertical-thumb"); ?>
-						</div>
-					</li>
-					<?php } ?>
-				</ul>
-			</div>
-		</div><!-- /carousel-large -->
+		
 		<div class="pt-3 text-center">
 			<a class="text-underline" href="<?php echo get_post_type_archive_link("circolare"); ?>"><strong>Vedi tutti</strong></a>
 		</div>
