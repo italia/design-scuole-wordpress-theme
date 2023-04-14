e2549848f1b83701b512f07a01576303e954a47a
Alessandro Magnaguagno <alessandro.magnaguagno@artigianelli.tn.it>
2023-04-14 12:26:44 +0200

    fix: organigramma

--- page-organizzazione.php
+++ page-organizzazione.php
@@ -10,10 +10,10 @@
 get_header();
 ?>
 
-<main id="main-container" class="main-container organigramma_page">
+<main id="main-container" class="main-container">
     <?php get_template_part("martini-template-parts/hero/hero_title"); ?>
     <section id="primary" class="container" >        
-        <div class="content mx-5 my-5" role="main" data-target="index" >
+        <div class="content mx-5 my-5 organigramma_page" role="main" data-target="index" >
         <?php the_content(); ?>
         </div><!-- end content -->
     </section><!-- end primary -->
