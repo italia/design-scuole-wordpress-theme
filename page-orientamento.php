9b2528a3a28fde684cd09338078e41e43f03616c
Federico <fede.toldo96@gmail.com>
2023-04-17 15:19:33 +0200

    fix link

--- page-orientamento.php
+++ page-orientamento.php
@@ -65,7 +65,7 @@
                     <p class="mb-4">Ecco qua la nostra Videoteca.</p>
                 </div>
                 <div class="col-12 col-md-4 d-flex align-items-center justify-content-end">
-                    <a class="btn-lg-default" href="http://martinomartini.local/videoteca/" target="blank">
+                    <a class="btn-lg-default" href="videoteca" target="blank">
                         <button class="w-auto">Vedi tutti i video</button>
                     </a>
                 </div>
