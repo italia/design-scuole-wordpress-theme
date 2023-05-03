27e4d9005bdae9b7b97a0b0b1989a8aa031484ec
Federico <fede.toldo96@gmail.com>
2023-05-03 10:44:15 +0200

    sportello cic e mensa
    sportello cic e mensa

--- page-sportello-cic.php
+++ page-sportello-cic.php
@@ -12,44 +12,25 @@ get_header();
 
     
 
-    <main id="main-container" class="main-container">
+<main id="main-container" class="main-container">
+       
+      
+       <?php get_template_part("martini-template-parts/hero/hero_title"); ?> 
 
-        <?php get_template_part("martini-template-parts/hero/hero_title"); ?>
 
-        <?php
-        while ( have_posts() ) :
-            the_post();
-            set_views($post->ID);
-            
-
-            $mail = get_post_meta("martini_email");
-            $phone = get_post_meta("martini_phone");
-            $documenti = get_post_meta("documents_download");
-            $link_url = get_post_meta("martini_url");
-            $ulteriori_informazioni = get_post_meta("martini_info");
-            $info_variabili_titolo = get_post_meta("martini_titolo");
-            $info_variabili = get_post_meta("martini_info_variable");
-            
-        ?>
-
-        <section id="text-block" class="section bg-white">
-            <div class="container">
-                <div class="row main-content variable-gutters">
-                    <div class="mb-5 pt-5 px-3 col-lg-8">
-                        
-                            <h2>Centro di Informazione e Consulenza</h2>
-                            <p>Il CIC (Centro di Informazione e Consulenza) è un servizio offerto a studenti, genitori e personale scolastico, curato e gestito dalla <strong>dott.ssa Francesca Fontana, psicologa e psicoterapeuta</strong> (Iscrizione all’Ordine della Provincia di Trento n. 349). <br>
-                                La consulenza psicologica si configura come uno <strong>spazio di ascolto</strong> e di <strong>confronto libero e gratuito</strong> in un <strong>luogo riservato</strong> e nel rispetto del <strong>segreto professionale.</strong> <br>
-                                L’accesso al servizio permetterà alla persona che richiederà il colloquio di trovare ascolto e riflettere con la psicologa su difficoltà e problematiche di varia natura (emotive, relazionali, scolastiche, familiari, ecc.) che possono creare disagio e malessere nella vita quotidiana sia a scuola sia nelle proprie relazioni. L’obiettivo è quello di aiutare a migliorare le proprie capacità di affrontare e risolvere le difficoltà.
-                            </p>
-
-                            <?php the_content(); ?>
-                        
-                    </div><!-- /col-lg-6 -->
-
-                    <div id="sidebar" class="col-lg-3 offset-lg-1 px-5 px-3 px-lg-3 py-5">
+    <section id="text-block" class="section bg-white">
+        <div class="container">
+            <div class="row main-content variable-gutters">
+                
+                <div class="col-lg-8 mt-5 mb-5">
+                    <div class="pt-5 px-3">                        
+                        <?php the_content(); ?>
+                </div><!-- content -->
+                 
+                <div id="sidebar" class="col-lg-3 offset-lg-1 px-5 px-3 px-lg-3 py-5">
                         <aside class="aside-main aside-sticky">
                             <div class="col-12 col-lg-9" id="program-legend">
+                                
 
                                 <!-- Altre informazioni -->
                                  <?php
@@ -66,32 +47,6 @@ get_header();
                                 }
                                 ?>
                                 <!-- //Altre informazioni -->
-
-                                <!-- Info variabili - titolo -->
-                                 <?php
-                                $info_variabili_titolo = get_post_meta( get_the_ID(), 'martini_titolo', true );
-                                if(trim($info_variabili_titolo) != ""){
-                                    ?>
-                                    <h5><?php echo wpautop($info_variabili_titolo); ?></h5> 
-                                    <?php
-                                }
-                                ?>
-                                <!-- //Info variabili - titolo -->
-
-                                <!-- Info variabili -->
-                                 <?php
-                                $info_variabili = get_post_meta( get_the_ID(), 'martini_info_variable', true );
-                                if(trim($info_variabili) != ""){
-                                    ?>
-                                    <div class="row variable-gutters">
-                                        <div class="col-12 wysiwig-text">
-                                            <?php echo wpautop($info_variabili); ?>
-                                        </div>
-                                    </div><!-- /row -->
-                                    <?php
-                                }
-                                ?>
-                                <!-- //Info variabili -->
 
                                 <!-- Campo contatti -->
                                     <div class="mailfield pb-1">
@@ -124,7 +79,7 @@ get_header();
                                         <?php } ?>
                                     </div>
                                     <!--/Campo contatti -->
-
+                                
                                 <!-- Campo modulistica -->
                                 <?php 
                                 $multidocuments_download = get_post_meta( get_the_ID(), 'documents_download', true );
@@ -156,24 +111,23 @@ get_header();
                                     <li>
                                          <a href="<?php echo $link_url;?>" target=blank> <?php echo $link_url;?> </a> 
                                     </li>
-                                    <?php }?>
-                                   
+                                    <?php }?>                         
                                 </ul>
                                 <?php } ?>
                                 <!--/Campo link -->
-
                             </div>
                         </aside>
-                    </div>  <!--/ sidebar -->
-                </div><!-- /row -->
-            </div><!-- /container -->
-        </section>
+                    </div> <!--/ sidebar -->
+
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </section>
 
 
 
-    </main>
+</main>
 
 
 <?php
-endwhile;
 get_footer();
+?>
