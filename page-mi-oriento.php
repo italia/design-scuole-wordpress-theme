c6d0917911b10f3431767ed3a684f251a152cfda
Federico <fede.toldo96@gmail.com>
2023-05-03 10:47:27 +0200

    sportello cic e mensa
    sportello cic e mensa

--- page-mi-oriento.php
+++ page-mi-oriento.php
@@ -24,6 +24,7 @@ get_header();
                 <div class="col-lg-8 mt-5 mb-5">
                     <div class="pt-5 px-3">                        
                         <?php the_content(); ?>
+                    </div>
                 </div><!-- content -->
                  
                 <div id="sidebar" class="col-lg-3 offset-lg-1 px-5 px-3 px-lg-3 py-5">
