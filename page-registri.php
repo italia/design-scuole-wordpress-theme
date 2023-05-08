672ace191698fe3217fdbe9a5066f0c52d0bbffe
Federico <fede.toldo96@gmail.com>
2023-05-08 10:55:19 +0200

    page registri
    page registri

---
+++ page-registri.php
@@ -0,0 +1,90 @@
+<?php
+get_header();
+?>
+
+<?php get_template_part("martini-template-parts/hero/hero_title"); ?>
+<main id="main-container" class="main-container">
+    <?php get_template_part("template-parts/common/breadcrumb"); ?>
+    <!-- loop per le pagine -->
+    <div class="container mb-5 martini-list">
+        <div>
+            <!-- Registro Docenti -->
+            <section class="martini-list--item px-0 py-4 align-items-center">
+                <div class="row container-martini px-0 px-2 align-items-center">
+                    <div class="col-12 col-md-6">
+                        <a href="https://martini-tn.registroelettronico.com/mastercom/">
+                            <h4 class="mb-0">Registro Docenti</h4>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-6 align-items-start text-md-right">
+                        <a class="btn-sm-default" href="https://martini-tn.registroelettronico.com/mastercom/">
+                            <button class="w-auto mt-3 mt-md-0 mb-0">Visita la pagina</button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <!-- Registro Serale -->
+            <section class="martini-list--item px-0 py-4 align-items-center">
+                <div class="row container-martini px-0 px-2 align-items-center">
+                    <div class="col-12 col-md-6">
+                        <a href="https://martini-tn-serale.registroelettronico.com/login/?next=/">
+                            <h4 class="mb-0">Registro Serale</h4>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-6 align-items-start text-md-right">
+                        <a class="btn-sm-default" href="https://martini-tn-serale.registroelettronico.com/login/?next=/">
+                            <button class="w-auto mt-3 mt-md-0 mb-0">Visita la pagina</button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <!-- Registro ASL -->
+            <section class="martini-list--item px-0 py-4 align-items-center">
+                <div class="row container-martini px-0 px-2 align-items-center">
+                    <div class="col-12 col-md-6">
+                        <a href="https://alternanza.registroelettronico.com/martini-tn/">
+                            <h4 class="mb-0">Registro ASL</h4>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-6 align-items-start text-md-right">
+                        <a class="btn-sm-default" href="https://alternanza.registroelettronico.com/martini-tn/">
+                            <button class="w-auto mt-3 mt-md-0 mb-0">Visita la pagina</button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <!-- Quaderno Studenti -->
+            <section class="martini-list--item px-0 py-4 align-items-center">
+                <div class="row container-martini px-0 px-2 align-items-center">
+                    <div class="col-12 col-md-6">
+                        <a href="https://martini-tn.registroelettronico.com/mastercom/">
+                            <h4 class="mb-0">Quaderno Studenti</h4>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-6 align-items-start text-md-right">
+                        <a class="btn-sm-default" href="https://martini-tn.registroelettronico.com/mastercom/">
+                            <button class="w-auto mt-3 mt-md-0 mb-0">Visita la pagina</button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <!-- Registro famiglie -->
+            <section class="martini-list--item px-0 py-4 align-items-center">
+                <div class="row container-martini px-0 px-2 align-items-center">
+                    <div class="col-12 col-md-6">
+                        <a href="https://martini-tn-sito.registroelettronico.com/login/?next=%2Fselect-student%2F">
+                            <h4 class="mb-0">Registro famiglie</h4>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-6 align-items-start text-md-right">
+                        <a class="btn-sm-default" href="https://martini-tn-sito.registroelettronico.com/login/?next=%2Fselect-student%2F">
+                            <button class="w-auto mt-3 mt-md-0 mb-0">Visita la pagina</button>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</main>
+<?php
+get_footer();
