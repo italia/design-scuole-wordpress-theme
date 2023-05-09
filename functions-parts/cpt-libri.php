81722daa678ed7ee73d9e807373f03c3d9df382a
Federico <fede.toldo96@gmail.com>
2023-05-09 13:28:41 +0200

    Page libri
    Page libri
    
    Signed-off-by: Federico <fede.toldo96@gmail.com>

--- functions-parts/cpt-libri.php
+++ functions-parts/cpt-libri.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * Definisce post type per Documenti Libri
+ * Definisce post type per libri
  */
 
 add_action( 'init', 'register_libri_post_type' );
 function register_libri_post_type() {
 
-	/** Documenti Libri **/
+	/** libri **/
 	$labels = array(
-		'name'          => _x( 'Libri', 'Post Type General Name', 'martino_martini' ),
-		'singular_name' => _x( 'Libri', 'Post Type Singular Name', 'martino_martini' ),
+		'name'          => _x( 'Libro', 'Post Type General Name', 'martino_martini' ),
+		'singular_name' => _x( 'Libro', 'Post Type Singular Name', 'martino_martini' ),
 		'add_new'       => _x( 'Aggiungi un File', 'Post Type Singular Name', 'martino_martini' ),
 		'add_new_item'  => _x( 'Aggiungi un File', 'Post Type Singular Name', 'martino_martini' ),
 		'edit_item'      => _x( 'Modifica il File', 'Post Type Singular Name', 'martino_martini' ),
@@ -22,8 +22,8 @@ function register_libri_post_type() {
 		'supports'      => array( 'title' ),
 		'hierarchical'  => true,
 		'public'        => true,
-		'menu_position' => 9,
-		'menu_icon'     => 'dashicons-book',
+		'menu_position' => 20,
+		'menu_icon'     => 'dashicons-book-alt',
 		'has_archive'   => true,
         'map_meta_cap'    => true,
 	);
@@ -31,61 +31,56 @@ function register_libri_post_type() {
 
 }
 
-//aggiunta categorie
-function libri_taxonomies() {
-    register_taxonomy(
-        'libri_tax',
-        'libri',
-        array(
-            'labels' => array(
-               'name' => 'Categorie',
-               'add_new_item' => 'Aggiungi nuova categoria',
-               'new_item_name' => "Nuova categoria"
-            ),
-            'show_ui' => true,
-            'show_tagcloud' => false,
-            'hierarchical' => true,
-            'show_admin_column' => true,
-            'show_in_rest' => true,
-            // 'show_in_quick_edit'         => false,
-            // 'meta_box_cb'                => false,
-        )
-    );
-
-}
-add_action( 'init', 'libri_taxonomies', 0 );
 
 // registro un nuovo field
 
-add_action('cmb2_init', 'martini_add_libri_metaboxes');
-function martini_add_libri_metaboxes()
-{
+add_action( 'cmb2_init', 'martini_add_libri_metaboxes' );
+function martini_add_libri_metaboxes() {
 
     $prefix = '_martini_libri_';
+    
+    $cmb_aftercontent = new_cmb2_box( array(
+    'id'           => $prefix . 'box_elementi_dati',
+    'title'        => __( 'Libri', 'martino_martini' ),
+    'object_types' => array( 'libri' ),
+    'context'      => 'normal',
+    'priority'     => 'high',
+    ) );
 
-    $cmb_aftercontent = new_cmb2_box(array(
-        'id'           => $prefix . 'box_elementi_dati',
-        'title'        => __('Archivio Documenti Libri', 'martino_martini'),
-        'object_types' => array('libri'),
-        'context'      => 'normal',
-        'priority'     => 'high',
-    ));
+    $cmb_aftercontent->add_field( array(
+        'name'             => 'Tipologia di documento',
+        'id'               => $prefix . 'wiki_test_radio', // da modificare e aggiungere il prefix
+        'type'             => 'radio',
+        'show_option_none' => false,
+        'options'          => array(
+            'biennio' => __( '1°Biennio (a carico delle famiglie)', 'cmb2' ),
+            'internazionale'    => __( 'Liceo Scientifico Internazionale delle Scienze Applicate', 'cmb2' ),
+            'applicate'    => __( 'Liceo Scientifico Op. Scienze Applicate', 'cmb2' ),
+            'sportivo'      => __( 'Liceo Scientifico Op. Sportivo', 'cmb2' ),
+            'sociale' => __( 'Liceo Scienze Umane Op. Economico Sociale', 'cmb2' ),
+            'diurno'    => __( 'Tecnico Economico Diurno', 'cmb2' ),
+            'serale'         => __( 'Tecnico Economico Serale', 'cmb2' ),
+            'tecnologico'      => __( 'Tecnico Tecnologico' ),
+        ),
+    ) );
 
-    // box per caricare file di libri
+    // box per caricare file del documento libri sulle scuole
 
-    $cmb_aftercontent->add_field(array(
+    $cmb_aftercontent->add_field( array(
         'id' => $prefix . 'file_libri',
-        'name'    => __('Carica file', 'martino_martini'),
-        'desc' => __('Archivio dei file Documenti Libri', 'martino_martini'),
+        'name'    => __( 'Carica file', 'martino_martini' ),
+        'desc' => __( 'Archivio file del documento libri della scuola' , 'martino_martini' ),
         'type' => 'file_list',
+        // 'preview_size' => array( 100, 100 ), // Default: array( 50, 50 )
+        // 'query_args' => array( 'type' => 'image' ), // Only images attachment
+        // Optional, override default text strings
         'text' => array(
-            'add_upload_files_text' => __('Aggiungi un nuovo file', 'martino_martini'), // default: "Add or Upload Files"
-            'remove_image_text' => __('Rimuovi file', 'martino_martini'), // default: "Remove Image"
-            'remove_text' => __('Rimuovi', 'martino_martini'), // default: "Remove"
+            'add_upload_files_text' => __('Aggiungi un nuovo allegato', 'martino_martini' ), // default: "Add or Upload Files"
+            'remove_image_text' => __('Rimuovi allegato', 'martino_martini' ), // default: "Remove Image"
+            'remove_text' => __('Rimuovi', 'martino_martini' ), // default: "Remove"
         ),
         'attributes' => array(
             'data-validation' => 'required',
         ),
-
-    ));
+    ) );
 }
