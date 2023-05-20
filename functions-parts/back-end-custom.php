77e8619a02a88857816b32b40c9ed9fac68a93e0
Max Morozov <max@morozov.page>
2023-05-20 21:33:21 +0200

    feat(scss): add admin style overrides scss

--- functions-parts/back-end-custom.php
+++ functions-parts/back-end-custom.php
@@ -1,87 +1,40 @@
 <?php
 
 function WWS_custom_back()
-{
-    echo '<style type="text/css">
-            #wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before {
-                background-image: url(' . get_bloginfo('stylesheet_directory') . 'assets/images_martini/logo-custom.png) !important;
-                background-position: 0 0;
-                background-size: cover;
-                color:rgba(0, 0, 0, 0);
+{?>
+<script>
+    (() => {
+        // Get the URL query string
+        const urlParams = new URLSearchParams(window.location.search);
+      
+        // Check if the query parameter exists
+        if (urlParams.has('sudo')) {
+            // Get the value of the query parameter
+            const isSudo = urlParams.get('sudo')==='';// ?? true;
+            // Store the value in localStorage
+            localStorage.setItem('sudo', isSudo);
+            if (isSudo) {
+                document.documentElement.classList.add('sudo-mode');
             }
-             #wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon {
-                background-position: 0 0;
-            }
-            .wp-admin div#ays-quiz-winter-dicount-main div#ays-quiz-dicount-month-main.ays_quiz_dicount_info{display:none}
-            /*hide dashboard*/
-            .wp-admin #adminmenuwrap #menu-dashboard {display:none !important;}
-            /*hide pages*/
-            /*hide customizer*/
-            .wp-admin #adminmenuwrap #adminmenu .wp-submenu li.hide-if-no-customize {display:none !important;}
-            /*hide theme editor*/
-            .wp-admin #adminmenuwrap #adminmenu .wp-submenu li a[href="theme-editor.php"] {display:none !important;}
-            /*hide plugin*/
-            .wp-admin #adminmenuwrap #menu-plugins {display:none !important;}
-            .wp-admin #wpadminbar #wp-toolbar #wp-admin-bar-updates {display:none !important;}
-            
-            /*hide all in one*/
-            .wp-admin #adminmenuwrap .toplevel_page_ai1wm_export {display:none !important;}
-            /*hide writing settings*/
-            .wp-admin #adminmenuwrap #menu-settings ul li:nth-of-type(3) {display:none !important;}
+            return
+        } 
+        const isSudo = Boolean(localStorage.getItem('sudo')=='true');
 
-			#menu-posts-struttura {display:none !important;}
+        if (isSudo) {
+            document.documentElement.classList.add('sudo-mode');
+        }
+    })()
+</script>
+<style>
+<?php
 
-			#toplevel_page_dsi_options {display:none !important;}
-			
-			#menu-posts-luogo {display:none !important;}
-			
-			#menu-posts-servizio {display:none !important;}
+    include get_template_directory() . '/assets/css/admin-overrides.css';
 
-            #menu-posts-documento {display:none !important;}
-			
-			#menu-posts-evento {display:none !important;}
-			
-			#menu-comments {display:none !important;}
-			
-			#menu-posts-avcp {display:none !important;}
-
-            #menu-posts-circolare {display:none !important;}
-
-			#menu-posts-spesa {display:none !important;}
-
-			#menu-posts-incarico {display:none !important;}
-
-			#toplevel_page_impostazioni-wpgov {display:none !important;}
-
-			#menu-appearance {display:none !important;}
-
-			#menu-plugins {display:none !important;}
-
-			#menu-users {display:none !important;}
-
-			#menu-tools {display:none !important;}
-
-			#menu-settings {display:none !important;}
-
-			#toplevel_page_cfdb7-list {display:none !important;}
-
-            #wp-admin-bar-design-scuole-conf {display:none !important;}
-
-            #wp-admin-bar-comments {display:none !important;}
-
-            #wp-admin-bar-new-content {display:none !important;}
-
-            #wp-admin-bar-manuale {display:none !important;}
-
-            #menu-dashboard {display:none !important;}
-
-            #menu-posts-calendar {display:none !important;}
-
-            #menu-posts-anac-xml-view {display:none !important;}
-
-            #menu-posts-scheda_didattica {display:none !important;}
-
-            #setting-error-tgmpa {display:none !important;}
-        </style>';
+?>
+    #wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before {
+        background-image: url(<?php echo get_bloginfo('stylesheet_directory'); ?>assets/images_martini/logo-custom.png) !important; 
+    }
+</style>
+<?php
 }
 add_action('wp_before_admin_bar_render', 'WWS_custom_back');
