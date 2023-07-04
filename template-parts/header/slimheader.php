6da504d0cf7a1e51fd6f5e69b0b8efa0d78711af
Francesco Improta <francesco@designabile.com>
2023-07-04 16:00:31 +0200

    Fixed columns arrangement

--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -1,12 +1,12 @@
 <div id="pre-header" class="bg-petrol">
     <div class="container">
         <div class="row variable-gutters">
-            <div class="col-6">
+            <div class="col-8">
                 <a href="https://www.miur.gov.it/" target="_blank" aria-label="MIUR - Collegamento esterno - Apre su nuova scheda">
                     <strong>Ministero dell'Istruzione e del Merito</strong>
                 </a>
             </div><!-- /col-6 -->
-            <div class="col-6 header-utils-wrapper">
+            <div class="col-4 header-utils-wrapper">
                 <div class="header-utils">
                     <?php
                     if(!is_user_logged_in()) {
