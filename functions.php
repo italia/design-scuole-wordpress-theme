91a33abb220e0c3e211e890180f6ef7b9de635c6
Sezione Sviluppo di Robyone <sviluppoRobyone@users.noreply.github.com>
2023-08-11 14:12:17 +0200

    Possibilità di personalizzare logo intestazione e favicon da configurazione (#346)
    Co-authored-by: Marco Rubin <marco.rubin@robyone.net>

--- functions.php
+++ functions.php
@@ -296,6 +296,15 @@ function add_menu_link_class( $atts, $item, $args ) {
   }
   add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
 
+function add_file_types_to_uploads($file_types){
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$new_filetypes['svgz'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes );
+	return $file_types;
+}
+
+add_action('upload_mimes', 'add_file_types_to_uploads');
 
 /**
  * Consenti ricerca per argomenti/tags con tutti i content types
