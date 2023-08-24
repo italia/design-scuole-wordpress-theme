0fa45a0dee36f3226ad3b54c4b5777b5d6a5bde8
Marco Rubin <marco.rubin@robyone.net>
2023-08-24 13:35:54 +0200

    Allineamento a codice tema nazionale

--- functions.php
+++ functions.php
@@ -296,6 +296,16 @@ function add_menu_link_class( $atts, $item, $args ) {
   }
   add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
 
+  function add_file_types_to_uploads($file_types){
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$new_filetypes['svgz'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes );
+	return $file_types;
+}
+
+add_action('upload_mimes', 'add_file_types_to_uploads');
+
 /**
  * Consenti ricerca per argomenti/tags con tutti i content types
  */
@@ -310,16 +320,6 @@ function add_tags_to_all_content_types( $query ) {
 }
 
 add_action( 'pre_get_posts', 'add_tags_to_all_content_types' );
-
-function add_file_types_to_uploads($file_types){
-	$new_filetypes = array();
-	$new_filetypes['svg'] = 'image/svg+xml';
-	$new_filetypes['svgz'] = 'image/svg+xml';
-	$file_types = array_merge($file_types, $new_filetypes );
-	return $file_types;
-}
-
-add_action('upload_mimes', 'add_file_types_to_uploads');
 
 // Sistema temporaneamente i breadcrumb per alcune pagine
 function breadcrumb_fix( $string, $arg1 ) {
