b46ac0140b8e5c390fc7a269b23f0a8a75002e32
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2023-09-11 16:42:05 +0200

    Immagine card correlati allineata a destra

--- template-parts/single/card-vertical-thumb.php
+++ template-parts/single/card-vertical-thumb.php
@@ -7,7 +7,7 @@ $image_url = get_the_post_thumbnail_url($post, "vertical-card");
 
 ?><div class="card card-bg card-vertical-thumb bg-white card-thumb-rounded">
 	<div class="card-body">
-		<div class="card-content">
+		<div class="card-content flex-grow-1">
 			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
 
 <?php
