9c6282c6c8b07ae262150d644dc48195a5ef920e
nonsonosperti <andrea.sperti@innoversa.it>
2023-11-22 19:18:03 +0100

    Aggiunta immagine del servizio

--- template-parts/servizio/card-noicon.php
+++ template-parts/servizio/card-noicon.php
@@ -3,11 +3,19 @@ global $servizio;
 if($servizio->post_status == "publish") {
     ?>
     <div class="card card-bg card-noicon rounded">
-        <a href="<?php echo get_permalink($servizio); ?>">
+        <a href="<?php echo get_permalink($servizio); ?>" aria-label="<?php echo $servizio->post_title; ?>">
             <div class="card-body">
-                <div class="card-icon-content" id="card-desc-<?php echo $servizio->ID; ?>">
-                    <p><strong><?php echo $servizio->post_title; ?></strong></p>
-                    <small><?php echo dsi_get_meta("sottotitolo", '_dsi_servizio_', $servizio->ID); ?></small>
+                <div class="card-icon-content" id="card-desc-<?php echo $servizio->ID; ?>"  style="text-align: center">
+					 <?php
+                                    $image_id= get_post_thumbnail_id($servizio);
+                                    if(has_post_thumbnail($servizio))
+                                        $image_url = get_the_post_thumbnail_url($servizio, "item-thumb");
+                                    else
+                                        $image_url = get_template_directory_uri() ."/assets/placeholders/logo-service.png";
+                                    dsi_get_img_from_id_url( $image_id, $image_url );
+                                ?>
+                    <?php /*<p><strong><?php echo $servizio->post_title; ?></strong></p>
+                    <small><?php echo dsi_get_meta("sottotitolo", '_dsi_servizio_', $servizio->ID); ?></small> */?>
                 </div><!-- /card-icon-content -->
             </div><!-- /card-body -->
         </a>
