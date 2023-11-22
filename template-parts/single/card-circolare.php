af3e0ef334f4fce5dc5628f42fb551f3d48917ec
nonsonosperti <andrea.sperti@innoversa.it>
2023-11-22 19:18:32 +0100

    Correzione card circolare per home

--- template-parts/single/card-circolare.php
+++ template-parts/single/card-circolare.php
@@ -4,11 +4,11 @@ $numerazione_circolare = dsi_get_meta("numerazione_circolare", "", $post->ID);
 $accesso_circolare = circolare_access($post->ID);
 ?>
 
-<div class="card card-bg bg-white card-thumb-rounded">
+<div class="card card-bg bg-white card-thumb-rounded col-lg-4">
 	<div class="card-body">
 		<div class="card-content">
 		<?php if($accesso_circolare != "false") { ?>
-			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
+			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>" aria-label="<?php echo $post->$post_title ?>"><?php echo get_the_title($post); ?></a></h3>
             <small class="h6 text-greendark"><?php _e("Circolare ", "design_scuole_italia"); echo $numerazione_circolare; ?></small>
 			<p><?php echo $post->_dsi_circolare_descrizione; ?></p>
 		<?php } else { ?>
