5e5c0728e49e73ca934e718be31a70d436779ab3
nonsonosperti <andrea.sperti@innoversa.it>
2023-11-26 16:22:20 +0100

    didattica

--- template-parts/home/didattica-cicli.php
+++ template-parts/home/didattica-cicli.php
@@ -148,7 +148,7 @@ if(is_array($scuole_didattica) && count($scuole_didattica)>0) {
                                     <p>Natascia Senatore</p>
 
                                     <p>
-                                        <a href="/la-scuola/" class="btn" style="background-color:#0a00cb; text-decoration:none;">Tutto sulla nostra Scuola</a>
+                                        <a href="/la-scuola/" class="btn" style="background-color:#cd5f4a; text-decoration:none; color: white">Tutto sulla nostra Scuola</a>
                                     </p>
                                 </div>
                             </div><!-- /accordion-large-content -->
