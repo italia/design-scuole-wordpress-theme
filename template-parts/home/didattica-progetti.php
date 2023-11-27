fb6290d1f1d7a1c92713d607e6f8459f82ead58b
nonsonosperti <andrea.sperti@innoversa.it>
2023-11-27 09:42:48 +0100

    progetti

--- template-parts/home/didattica-progetti.php
+++ template-parts/home/didattica-progetti.php
@@ -31,7 +31,7 @@ if(is_array($posts) && count($posts)) {
 
             foreach ($posts as $progetto) { ?>
                 <div class="col-lg-4">
-                    <?php get_template_part("template-parts/progetto/card"); ?>
+                    <?php get_template_part("template-parts/progetto/card", "bluelight"); ?>
                 </div>
             <?php } ?>
         </div><!-- /row -->
