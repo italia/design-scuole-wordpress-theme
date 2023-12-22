dc23d24cae4df881ba89c27ef2a5e2834a127895
rcoopcsc <125898973+rcoopcsc@users.noreply.github.com>
2023-12-22 15:56:59 +0100

    fix: show document type

--- single-documento.php
+++ single-documento.php
@@ -62,10 +62,20 @@ $user_can_view_post = dsi_members_can_user_view_post(get_current_user_id(), $pos
                             <?php get_template_part( "template-parts/single/actions" ); ?>
                             <?php
                                 $badgeclass = "badge-outline-redbrown";
-                                get_template_part( "template-parts/common/badges-argomenti" ); ?>
-
-                        </div><!-- /col-lg-3 col-md-4 offset-lg-1 -->
-                    </div><!-- /row -->
+                                get_template_part( "template-parts/common/badges-argomenti" ); 
+                            ?>
+                        <div class="badges-wrapper badges-main mt-4">
+                            <?php $post_tags = get_the_terms(get_the_ID(), 'tipologia-documento');
+                                if ($post_tags) {
+                                    echo '<h2 class="h4">Tipologia</h2>';
+                                    foreach($post_tags as $tag) {
+                                        echo '<a href="'.get_tag_link($tag->term_id).'" class="badge badge-sm badge-pill badge-outline-redbrown" aria-label="Tipologia: '.$tag->name.'">'. $tag->name .'</a> ';
+                                    }
+                                }
+                            ?>
+				        </div>		
+                    </div><!-- /col-lg-3 col-md-4 offset-lg-1 -->
+                </div><!-- /row -->
                 </div><!-- /container -->
             </section><!-- /section -->
 
