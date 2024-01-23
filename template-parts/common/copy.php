27724eaadbac0e6feef8e852f27a104ca7d9fb9a
Marco Rubin <marco.rubin@robyone.net>
2024-01-23 14:56:24 +0100

    Aggiornamento link e traduzione credits

--- template-parts/common/copy.php
+++ template-parts/common/copy.php
@@ -1,5 +1,5 @@
 <div class="row variable-gutters mb-3">
     <div class="col-lg-10 offset-lg-1 text-right footer-link">
-        <a href="https://designers.italia.it/kit/scuole/" aria-label="Concept &amp; Design by Designers Italia" target="_blank">Concept & Design by Designers Italia</a>
+        <a href="https://designers.italia.it/modelli/scuole/" aria-label="Idea e progetto di Designers Italia" target="_blank">Idea e progetto di Designers Italia</a>
     </div>
 </div><!-- /row -->
