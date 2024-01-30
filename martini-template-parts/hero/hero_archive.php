a5f6f9c08dcbaad4f55a3da0bb9cad8b3f718001
Federico <fede.toldo96@gmail.com>
2024-01-30 10:30:48 +0100

    fix hero bg

--- martini-template-parts/hero/hero_archive.php
+++ martini-template-parts/hero/hero_archive.php
@@ -3,7 +3,7 @@
         <div class="h-100 align-items-center">
             <div id="hero-content" class="p-2 d-flex flex-column justify-content-center">
                 <?php the_archive_title('<h1 class="p-0 mb-2">', '</h1>'); ?>
-                <div class="hero-img" style="background-image: none; background-color: red;"></div>
+                <div class="hero-img" style="background-image: none; background-color: url('https://www.martinomartini.eu/wp-content/uploads/2023/05/wesley-tingey-snNHKZ-mGfE-unsplash.jpg');"></div>
             </div>
         </div>
     </section>
--- martini-template-parts/hero/hero_archive_doc.php
+++ martini-template-parts/hero/hero_archive_doc.php
@@ -4,7 +4,7 @@
             <div id="hero-content" class="p-2 d-flex flex-column justify-content-center">
                 <h1 class="p-0 mb-2"><?php _e("Le carte della Scuola", "design_scuole_italia"); ?></h1>
                 <h2 class="h4 font-weight-normal"><?php echo $testo_sezione_documenti; ?></h2>
-                <div class="hero-img" style="background-image: none; background-color: red;"></div>
+                <div class="hero-img" style="background-image: none; background-color: url('https://www.martinomartini.eu/wp-content/uploads/2023/05/wesley-tingey-snNHKZ-mGfE-unsplash.jpg');"></div>
             </div>
         </div>
     </section>
