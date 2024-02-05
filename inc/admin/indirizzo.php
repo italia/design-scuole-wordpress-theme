02ba847552f73afba3d0ce95544b1e3c191912bd
Marco Rubin <marco.rubin@robyone.net>
2024-02-05 13:05:41 +0100

    Campi procedure, orario, libri, casi particolari
    Editor wysiwyg

--- inc/admin/indirizzo.php
+++ inc/admin/indirizzo.php
@@ -200,7 +200,12 @@ function dsi_add_indirizzo_metaboxes() {
         'id' => $prefix . 'procedura_esito',
         'name'        => __( 'Procedure collegate', 'design_scuole_italia' ),
         'desc' => __( 'Questo campo indica eventuali procedure collegate.' , 'design_scuole_italia' ),
-        'type' => 'textarea'
+        'type' => 'wysiwyg',
+        'options' => array(
+            'media_buttons' => false, // show insert/upload button(s)
+            'textarea_rows' => 4, // rows="..."
+            'teeny' => true, // output the minimal editor config used in Press This
+        ),
     ) );
 
     $cmb_undercontent->add_field( array(
@@ -316,8 +321,12 @@ function dsi_add_indirizzo_metaboxes() {
         'id' => $prefix . 'calendario_classi_descrizione',
         'name'        => __( 'L\'orario delle classi', 'design_scuole_italia' ),
         'desc' => __( 'Testo introduttivo' , 'design_scuole_italia' ),
-        'type' => 'textarea',
-
+        'type' => 'wysiwyg',
+        'options' => array(
+            'media_buttons' => false, // show insert/upload button(s)
+            'textarea_rows' => 4, // rows="..."
+            'teeny' => true, // output the minimal editor config used in Press This
+        ),
     ) );
     $cmb_undercontent->add_field( array(
         'id' => $prefix . 'calendario_classi_file',
@@ -337,7 +346,12 @@ function dsi_add_indirizzo_metaboxes() {
         'id' => $prefix . 'libri_testo_descrizione',
         'name'        => __( 'Libri di testo', 'design_scuole_italia' ),
         'desc' => __( 'Testo introduttivo' , 'design_scuole_italia' ),
-        'type' => 'textarea',
+        'type' => 'wysiwyg',
+        'options' => array(
+            'media_buttons' => false, // show insert/upload button(s)
+            'textarea_rows' => 4, // rows="..."
+            'teeny' => true, // output the minimal editor config used in Press This
+        ),
     ) );
     $cmb_undercontent->add_field( array(
         'id' => $prefix . 'libri_testo_file',
@@ -431,8 +445,12 @@ function dsi_add_indirizzo_metaboxes() {
         'id' => $prefix . 'casi_particolari',
         'name'        => __( 'Casi particolari', 'design_scuole_italia' ),
         'desc' => __( 'Inserire come testo libero, eventuali casi particolari riferiti all\'ottenimento del Servizio in questione.' , 'design_scuole_italia' ),
-        'type' => 'textarea',
-
+        'type' => 'wysiwyg',
+        'options' => array(
+            'media_buttons' => false, // show insert/upload button(s)
+            'textarea_rows' => 4, // rows="..."
+            'teeny' => true, // output the minimal editor config used in Press This
+        ),
     ) );
 
     $cmb_undercontent->add_field( array(
