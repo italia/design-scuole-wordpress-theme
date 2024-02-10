9791a2d121a1faa0a9401ab35a89d879f5727af1
rcoopcsc <125898973+rcoopcsc@users.noreply.github.com>
2024-02-10 10:47:16 +0100

    feat: refactoring "panoramica didattica" (#568)

--- template-parts/home/didattica-schede.php
+++ template-parts/home/didattica-schede.php
@@ -7,21 +7,21 @@ $posts = get_posts($args);
 if(is_array($posts) && count($posts)) {
 
     ?>
-    <section class="section bg-bluelectricdark py-5">
+    <section class="section bg-light py-5">
         <div class="container">
             <div class="row variable-gutters">
                 <div class="col">
                     <div class="section-title mb-5">
-                        <h3><?php _e("Schede didattiche", "design_scuole_italia"); ?></h3>
+                        <h2><?php _e("Le schede didattiche", "design_scuole_italia"); ?></h2>
                     </div>
 
-                    <div class="splide splide-double mb-5" data-bs-carousel-splide>
-                        <div class="splide__track">
+                    <div class="splide splide-double mb-1" data-bs-carousel-splide>
+                        <div class="splide__track" >
                             <ul class="splide__list">
                                 <?php
                                 foreach ($posts as $post) { ?>
-                                    <li class="splide__slide">
-                                        <div class="item">
+                                    <li class="splide__slide mb-0">
+                                        <div class="item rounded">
                                             <?php get_template_part("template-parts/didattica/card"); ?>
                                         </div>
                                     </li>
@@ -33,7 +33,7 @@ if(is_array($posts) && count($posts)) {
             </div><!-- /row -->
 
         </div><!-- /container -->
-        <div class="pb-5 text-center mt-4">
+        <div class="pb-5 text-center mt-2">
             <a class="text-underline" href="<?php echo get_post_type_archive_link("scheda_didattica") ?>"><strong><?php _e("Vedi tutte le schede didattiche", "design_scuole_italia"); ?></strong></a>
         </div>
     </section><!-- /section --><?php
