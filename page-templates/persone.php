18c4b425de7565ae34b51195831b4533ffbb2d0a
dgmorgagni <79083735+dgmorgagni@users.noreply.github.com>
2024-03-16 11:55:03 +0100

    Update persone.php
    Ordinamento persone in base al cognome e razionalizzazione pagina

--- page-templates/persone.php
+++ page-templates/persone.php
@@ -8,79 +8,54 @@
 global $post;
 get_header();
 
+function visualizza_utenti($ruolo_utente, $intestazione_sezione) {
+    // carica tutti gli utenti e li ordina per cognome
+	$args = array(
+        'meta_key' => 'last_name', 
+        'orderby' => 'meta_value',
+        'order' => 'ASC', 
+        //seleziona gli utenti in base al ruolo
+	'meta_query' => array(
+            'relation' => 'AND',
+            array(
+                'key' => '_dsi_persona_ruolo_scuola',
+                'value' => $ruolo_utente,
+                'compare' => '='
+            )
+        )
+    );
+$users = get_users($args);
+    $number_of_users = count($users);
+    if ($number_of_users > 0) {
+        ?>
+        <section class="section mt-4">
+            <div class="container">
+                <div class="title-section mb-5">
+                <h2 class="h4"><?php echo $intestazione_sezione; ?></h2>
+                </div>
+            <div class="row variable-gutters">
+            <?php
+                foreach ($users as $user) {
+                    global $autore; 
+                    $autore = get_user_by("ID", $user->ID); 
+                    get_template_part("template-parts/autore/card-persona");
+                }
+            ?>
+            </div>      
+        </section>  
+        <?php
+    }
+}
+
 ?>
+	
 <main id="main-container" class="main-container redbrown">
-	
-	<?php get_template_part("template-parts/common/breadcrumb"); ?>
-	
-	<?php get_template_part("template-parts/hero/persone"); ?>
 
-<?php 
-$args = array('meta_key' => '_dsi_persona_ruolo_scuola','meta_value'	=>	'dirigente'); 			
-$users = get_users($args);
-$number_of_users = count($users); 
-if ($number_of_users > 0) {
-?>	
-	<section class="section mt-4">
-		<div class="container">
-			<div class="title-section mb-5">
-			<h2 class="h4">Dirigente Scolastico</h2>
-			</div>	
-		<div class="row variable-gutters">	
-		<?php  
-			foreach($users as $user){
-			$autore = get_user_by("ID", $user->data->ID);
-			get_template_part("template-parts/autore/card-persona");
-			}
-		?>
-		</div>		
-	</section>					
-<?php } ?>
-
-<?php 
-$args = array('meta_key' => '_dsi_persona_ruolo_scuola','meta_value'	=>	'docente'); 			
-$users = get_users($args);
-$number_of_users = count($users); 
-if ($number_of_users > 0) { 
-?>				
-	<section class="section mt-4">
-		<div class="container">
-			<div class="title-section mb-5">
-			<h2 class="h4">Personale docente</h2>
-			</div>		
-		<div class="row variable-gutters">	
-		<?php  
-			foreach($users as $user){
-			$autore = get_user_by("ID", $user->data->ID);
-			get_template_part("template-parts/autore/card-persona");
-			}
-		?>
-		</div>		
-	</section>	
-<?php } ?>
-
-
-<?php 
-$args = array('meta_key' => '_dsi_persona_ruolo_scuola','meta_value'	=>	'personaleata'); 			
-$users = get_users($args);
-$number_of_users = count($users); 
-if ($number_of_users > 0) {
-?>
-	<section class="section mt-4">
-		<div class="container">
-			<div class="title-section mb-5">
-			<h2 class="h4">Personale non docente</h2>
-			</div>	
-		<div class="row variable-gutters">	
-		<?php  
-			foreach($users as $user){
-			$autore = get_user_by("ID", $user->data->ID);
-			get_template_part("template-parts/autore/card-persona");
-			}
-		?>
-		</div>		
-	</section>	
-<?php } ?>
+    <?php get_template_part("template-parts/common/breadcrumb"); ?>
+    <?php get_template_part("template-parts/hero/persone"); ?>
+    <?php visualizza_utenti('dirigente', 'Dirigente Scolastico'); ?>
+    <?php visualizza_utenti('docente', 'Personale docente'); ?>
+    <?php visualizza_utenti('personaleata', 'Personale non docente'); ?>
 
 </main>
 
