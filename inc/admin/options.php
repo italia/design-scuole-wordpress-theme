99a82aa8e0374299556f0e3077b54859b278ea54
rcoopcscs <rdolce@coopcsc.it>
2024-04-12 10:33:45 +0200

    Correzione testi descrittivi nel backend

--- inc/admin/options.php
+++ inc/admin/options.php
@@ -1060,7 +1060,7 @@ function dsi_register_main_options_metabox() {
     $notizie_options->add_field( array(
 		'id' => $prefix . 'testo_notizie',
 		'name'        => __( 'Descrizione Sezione Notizie', 'design_scuole_italia' ),
-		'desc' => __( 'es: "Le notizie del liceo scientifico Enriques dedicate a tutti i genitori, studenti, personale ATA e docenti"' , 'design_scuole_italia' ),
+		'desc' => __( 'es: "Le notizie della scuola dedicate a tutti i genitori, studenti, personale ATA e docenti".<br>Il testo compare nella pagina della <a href="'.$notizie_landing_url.'">pagina di panoramica delle Novità</a>. Max 140 caratteri.' , 'design_scuole_italia' ),
 		'type' => 'textarea',
 		'attributes'    => array(
 			'maxlength'  => '140'
@@ -1070,7 +1070,7 @@ function dsi_register_main_options_metabox() {
     $notizie_options->add_field( array(
 		'id' => $prefix . 'testo_eventi',
 		'name'        => __( 'Descrizione Sezione Calendario eventi', 'design_scuole_italia' ),
-		'desc' => __( 'es: "Calendario scolastico"' , 'design_scuole_italia' ),
+		'desc' => __( 'es: "Calendario scolastico"<br>Il testo compare nella pagina <a href="'.get_post_type_archive_link("evento").'">Calendario evento</a>. Max 140 caratteri.' , 'design_scuole_italia' ),
 		'type' => 'textarea',
 		'attributes'    => array(
 			'maxlength'  => '140'
