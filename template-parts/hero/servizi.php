fc362eb84b5f5c98117857958e60ac4cc9b93de1
nonsonosperti <andrea.sperti@innoversa.it>
2024-04-15 16:33:13 +0200

    color update

--- template-parts/hero/servizi.php
+++ template-parts/hero/servizi.php
@@ -4,8 +4,8 @@ global $post;
 $testo_servizi = dsi_get_option("testo_servizi", "servizi");
 
 ?>
-<section class="section bg-purplelight bg-purplegradient py-5 position-relative d-flex align-items-center overflow-hidden" >
-	<div class="purple-oval-forms">
+<section class="section py-5 position-relative d-flex align-items-center overflow-hidden" >
+	<!-- <div class="purple-oval-forms">
 		<svg width="100%" height="100%" viewBox="0 0 578 359" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" role="img" aria-label="purple oval forms">
               <g id="Group-2">
 	              <path id="Oval-2" d="M578,359c0,-159.61 -129.39,-289 -289,-289c-159.61,0 -289,129.39 -289,289l578,0Z" style="fill:url(#_Linear1);fill-rule:nonzero;"/>
@@ -22,7 +22,7 @@ $testo_servizi = dsi_get_option("testo_servizi", "servizi");
 				</linearGradient>
 			</defs>
             </svg>
-	</div>
+	</div> -->
 	<div class="container">
 		<div class="row variable-gutters">
 			<div class="col-md-5">
