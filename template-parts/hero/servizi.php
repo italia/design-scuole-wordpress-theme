e7ae321d7f04196080d57baf5fcd6c61f8e5983a
nonsonosperti <andrea.sperti@innoversa.it>
2024-04-15 16:34:26 +0200

    color update

--- template-parts/hero/servizi.php
+++ template-parts/hero/servizi.php
@@ -4,7 +4,7 @@ global $post;
 $testo_servizi = dsi_get_option("testo_servizi", "servizi");
 
 ?>
-<section class="section py-5 position-relative d-flex align-items-center overflow-hidden" >
+<section class="section bg-orange py-5 position-relative d-flex align-items-center overflow-hidden" >
 	<!-- <div class="purple-oval-forms">
 		<svg width="100%" height="100%" viewBox="0 0 578 359" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" role="img" aria-label="purple oval forms">
               <g id="Group-2">
