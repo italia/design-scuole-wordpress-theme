7a3da22c627548e08fc1ae7a575d689674d7441c
nonsonosperti <andrea.sperti@innoversa.it>
2024-04-15 18:35:43 +0200

    color update

--- template-parts/home/didattica-cicli.php
+++ template-parts/home/didattica-cicli.php
@@ -116,7 +116,7 @@ if(is_array($scuole_didattica) && count($scuole_didattica)>0) {
                                         ?>
 
                                         <div class="text-center text-sm-left">
-                                            <a class="btn mt-4 mb-2" href="<?php echo get_permalink($idstruttura); ?>" style="background-color: #0a00cb; color: white"><?php _e("Vai alla presentazione della scuola", "design_scuole_italia"); ?></a>
+                                            <a class="btn mt-4 mb-2" href="<?php echo get_permalink($idstruttura); ?>"><?php _e("Vai alla presentazione della scuola", "design_scuole_italia"); ?></a>
                                         </div>
 
                                     </div>
@@ -156,13 +156,13 @@ if(is_array($scuole_didattica) && count($scuole_didattica)>0) {
 
                             <div id="tab-iscrizione" class="responsive-tabs-content">
                                 <div class="accordion-large accordion-wrapper">
-                                <h3 class="mb-0" style="color: white">Come iscriversi</h3>
+                                <h3 class="mb-0">Come iscriversi</h3>
 
                                 <p>Le Iscrizioni On Line prevedono alcune fasi:</p>
 
                                 <ul style="background-color: transparent !important">
-                                    <li><strong>PRIMA FASE:</strong> abilitazione al servizio presso il portale del Ministero dell'Istruzione e del Merito <a href="https://www.miur.gov.it/" style="color: white; text-decoration: underline">https://www.miur.gov.it</a> dal 19 dicembre 2022. I genitori e gli esercenti la responsabilità genitoriale (affidatari, tutori) si abilitano al Servizio “Iscrizioni on line”, disponibile sul portale Ministero dell’Istruzione e del Merito :<a href="http://www.istruzione.it/iscrizionionline/" style="color: white; text-decoration: underline">www.istruzione.it/iscrizionionline/</a> utilizzando le credenziali SPID (Sistema Pubblico di Identità Digitale), CIE (Carta di identità elettronica) o eIDAS (Electronic IDentification Authentication and Signature) .</li>
-                                    <li><strong>SECONDA FASE:</strong> compilazione della domanda vera e propria e l'invio alle scuole scelte  utilizzando una procedura accessibile all'indirizzo: <a href="https://www.istruzione.it/iscrizionionline/" style="color: white; text-decoration: underline">https://www.istruzione.it/iscrizionionline/</a> [si accede tramite SPID/CIE/eIDAS] dal ore dalle 8:00 del 9 gennaio 2023 alle 20:00 del 30 gennaio 2023. </li>
+                                    <li><strong>PRIMA FASE:</strong> abilitazione al servizio presso il portale del Ministero dell'Istruzione e del Merito <a href="https://www.miur.gov.it/" style="text-decoration: underline">https://www.miur.gov.it</a> dal 19 dicembre 2022. I genitori e gli esercenti la responsabilità genitoriale (affidatari, tutori) si abilitano al Servizio “Iscrizioni on line”, disponibile sul portale Ministero dell’Istruzione e del Merito :<a href="http://www.istruzione.it/iscrizionionline/" style="text-decoration: underline">www.istruzione.it/iscrizionionline/</a> utilizzando le credenziali SPID (Sistema Pubblico di Identità Digitale), CIE (Carta di identità elettronica) o eIDAS (Electronic IDentification Authentication and Signature) .</li>
+                                    <li><strong>SECONDA FASE:</strong> compilazione della domanda vera e propria e l'invio alle scuole scelte  utilizzando una procedura accessibile all'indirizzo: <a href="https://www.istruzione.it/iscrizionionline/" style="text-decoration: underline">https://www.istruzione.it/iscrizionionline/</a> [si accede tramite SPID/CIE/eIDAS] dal ore dalle 8:00 del 9 gennaio 2023 alle 20:00 del 30 gennaio 2023. </li>
                                     <li><strong>TERZA FASE:</strong> Aggiornamento sullo stato di avanzamento della domanda ed eventuale accettazione da parte della Scuola successivamente alla scadenza della procedura di iscrizione on line.</li>
                                 </ul>
 
