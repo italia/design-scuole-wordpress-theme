0b2ad4bafc0cec159ae47f5bfc028b68466f5d0c
nonsonosperti <andrea.sperti@innoversa.it>
2024-05-01 00:23:55 +0200

    thumbnail changes

--- template-parts/single/card-vertical-thumb.php
+++ template-parts/single/card-vertical-thumb.php
@@ -7,6 +7,11 @@ $image_url = get_the_post_thumbnail_url($post, "vertical-card");
 
 ?><div class="card card-bg card-vertical-thumb bg-white card-thumb-rounded <?php echo $set_card_top_margin ? "mt-2" : ""; ?>">
 	<div class="card-body">
+    <?php if($image_url) { ?>
+			<div class="card-thumb">
+            	<?php dsi_get_img_from_id_url( $image_id, $image_url ); ?>
+			</div>
+		<?php  } ?>
 		<div class="card-content flex-grow-1">
 			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
 
@@ -26,10 +31,6 @@ $image_url = get_the_post_thumbnail_url($post, "vertical-card");
         }
             ?>
 		</div>
-		<?php if($image_url) { ?>
-			<div class="card-thumb">
-            	<?php dsi_get_img_from_id_url( $image_id, $image_url ); ?>
-			</div>
-		<?php  } ?>
+		
 	</div><!-- /card-body -->
 </div><!-- /card --><?php
