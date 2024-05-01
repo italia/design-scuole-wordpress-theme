42fc05ae2fd3a51cd5aaf801c8e3091757619f58
nonsonosperti <andrea.sperti@innoversa.it>
2024-05-01 02:09:52 +0200

    test servizi

--- template-parts/servizio/card-noicon.php
+++ template-parts/servizio/card-noicon.php
@@ -14,7 +14,7 @@ if($servizio->post_status == "publish") {
                                         $image_url = get_template_directory_uri() ."/assets/placeholders/logo-service.png";
                                     dsi_get_img_from_id_url( $image_id, $image_url );
                                 ?>
-                    <?php /*<p><strong><?php echo $servizio->post_title; ?></strong></p>
+                    <p><strong><?php echo $servizio->post_title; ?></strong></p>
                     <small><?php echo dsi_get_meta("sottotitolo", '_dsi_servizio_', $servizio->ID); ?></small> */?>
                 </div><!-- /card-icon-content -->
             </div><!-- /card-body -->
