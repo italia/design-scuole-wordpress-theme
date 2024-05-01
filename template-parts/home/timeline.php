aba0d7e77285745ad661cdc32aba2cac4b9f1502
nonsonosperti <andrea.sperti@innoversa.it>
2024-05-01 14:24:42 +0200

    scuola

--- template-parts/home/timeline.php
+++ template-parts/home/timeline.php
@@ -4,13 +4,13 @@
 $timeline = dsi_get_option( "timeline", "la_scuola" );
 if(is_array($timeline) && count($timeline) > 0) {
 	?>
-	<section class="section section-padding bg-blue-dark">
+	<section class="section section-padding bg-bluelectric">
 		<div class="container">
 			<div class="row variable-gutters mt-0 mt-xl-2">
 				<div class="col">
 					<div class="title-section text-center mb-5">
-						<h3 class="mb-2" style="color: #ffffff;"><?php _e( "La storia della scuola", "design_scuole_italia" ); ?></h3>
-						<p style="color: #ffffff;"><?php echo dsi_get_option( "descrizione_scuola", "la_scuola" ); ?></p>
+						<h3 class="mb-2" style="color: #000;"><?php _e( "La storia della scuola", "design_scuole_italia" ); ?></h3>
+						<p style="color: #000;"><?php echo dsi_get_option( "descrizione_scuola", "la_scuola" ); ?></p>
 					</div><!-- /title-section -->
 				</div><!-- /col -->
 			</div><!-- /row -->
