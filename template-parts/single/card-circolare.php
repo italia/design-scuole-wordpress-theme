939d81e2c9e6cbacef1a676dbe604ffd37237f56
nonsonosperti <andrea.sperti@innoversa.it>
2024-05-01 18:00:36 +0200

    titoli

--- template-parts/single/card-circolare.php
+++ template-parts/single/card-circolare.php
@@ -8,7 +8,7 @@ $accesso_circolare = circolare_access($post->ID);
 	<div class="card-body">
 		<div class="card-content">
 		<?php if($accesso_circolare != "false") { ?>
-			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>" aria-label="<?php echo $post->$post_title ?>"><?php echo get_the_title($post); ?></a></h3>
+			<h3 class="h5" style="font-weight: 600"><a href="<?php echo get_permalink($post); ?>" aria-label="<?php echo $post->$post_title ?>"><?php echo get_the_title($post); ?></a></h3>
             <small class="h6 text-greendark"><?php _e("Circolare ", "design_scuole_italia"); echo $numerazione_circolare; ?></small>
 			<p><?php echo $post->_dsi_circolare_descrizione; ?></p>
 		<?php } else { ?>
--- template-parts/single/card.php
+++ template-parts/single/card.php
@@ -4,7 +4,7 @@ global $post;
 <div class="card card-bg bg-white card-thumb-rounded">
 	<div class="card-body">
 		<div class="card-content">
-			<h3 class="h5"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
+			<h3 class="h5" style="font-weight: 600"><a href="<?php echo get_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h3>
       <p><?php echo dsi_get_meta("descrizione", "", $post->ID) ?  dsi_get_meta("descrizione", "", $post->ID) :  get_the_excerpt($post); ?></p>
 		</div>
 	</div><!-- /card-body -->
