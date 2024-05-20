289869299e22f57cafab3cc75a16fe88eb320a53
nonsonosperti <andrea.sperti@innoversa.it>
2024-05-20 09:47:13 +0200

    icona

--- template-parts/svg/icona-scuola.php
+++ template-parts/svg/icona-scuola.php
@@ -4,7 +4,7 @@ if(!$icon_color)
     $icon_color = "#F0A18A";
 
 if(!isset($second_icon_color))
-	$second_icon_color = "#000";
+	$second_icon_color = "#EA7653";
 ?>
 
 <svg class="icon" width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;"  aria-hidden="true">
