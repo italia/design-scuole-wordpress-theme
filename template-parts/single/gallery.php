6d46e3c41f93de81a5346c16b56510473aa061c0
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2024-05-31 14:34:26 +0200

    feat: link a video nella galleria multimediale

--- template-parts/single/gallery.php
+++ template-parts/single/gallery.php
@@ -10,10 +10,36 @@ foreach ($gallery as $ida=>$urlg){
 	<li class="splide__slide">
 		<div class="it-single-slide-wrapper gallery-item h-100">
 				<figure>
-					<a href="<?php echo $urlg; ?>" aria-label="Visualizza foto: <?php echo $attach->post_title; ?>">
-						<?php dsi_get_img_from_url($urlg); ?>
-					</a>
 					<?php
+						if (wp_attachment_is('video', $ida)) {
+							$image_url = get_the_post_thumbnail_url($ida) ?: get_the_post_thumbnail_url();
+						?>
+							<a href="<?php echo $urlg; ?>" target="_blank" aria-label="Visualizza video: <?php echo $attach->post_title; ?>">
+								<div class="video-wrapper shadow-none rounded-0">
+									<?php
+									if($image_url){
+										dsi_get_img_from_url($image_url);
+									}
+									else
+									{
+									?>
+										<div class="bg-secondary" style="height: 320px"></div>
+										<?php
+									}
+									?>
+									<svg class="svg-play">
+										<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-play"></use>
+									</svg>
+								</div>
+							</a>
+						<?php
+						} else {
+						?>
+							<a href="<?php echo $urlg; ?>" aria-label="Visualizza foto: <?php echo $attach->post_title; ?>">
+								<?php dsi_get_img_from_url($urlg); ?>
+							</a>
+						<?php
+						}
 					if (!empty($attach->post_excerpt)) {
 					?>
 						<figcaption><?php echo $attach->post_excerpt; ?></figcaption>
