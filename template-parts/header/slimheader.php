55c2e3cd366518313f3c5319afc89255bbf2c727
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-06 19:50:43 +0200

    header

--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -3,7 +3,7 @@
         <div class="row variable-gutters">
             <div class="col-8">
                 <a href="https://www.miur.gov.it/" target="_blank" aria-label="MIUR - Collegamento esterno - Apre su nuova scheda">
-                    <strong>Ministero dell'Istruzione e del Merito</strong>
+                    <strong>MIUR</strong>
                 </a>
             </div><!-- /col-6 -->
             <div class="col-4 header-utils-wrapper">
