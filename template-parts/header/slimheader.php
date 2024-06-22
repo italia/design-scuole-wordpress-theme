bb6ab77a93aec4f7fe5b4a007e4dcb436d38ed95
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-23 01:34:24 +0200

    slimheader

--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -36,12 +36,12 @@
                     <strong>MIUR</strong>
                 </a>
                 <a href="mailto:KRPC02000L@istruzione.it" style="margin-right: 10px" target="_blank" aria-label="Indirizzo email del liceo">
-                    <strong> <i class="fa fa-envelope" aria-hidden="true"></i> KRPC02000L@istruzione.it</strong>
+                    <strong> <i class="fa fa-envelope" aria-hidden="true"></i></strong>
                 </a>
                 <a href="tel:00390962905731" style="margin-right: 10px" target="_blank" aria-label="Numero di telefono del liceo">
-                    <strong> <i class="fa fa-phone" aria-hidden="true"></i> 0962905731</strong>
+                    <strong> <i class="fa fa-phone" aria-hidden="true"></i> </strong>
                 </a>
-                <span>Codice Meccanografico KRPC02000L  Codice Fatturazione UFHY7G</span>
+                
             </div><!-- /col-6 -->
             <div class="col-4 header-utils-wrapper">
                 <div class="header-utils">
