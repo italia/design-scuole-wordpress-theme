a38837daa008ad70d405c0115f241a91a5fb5701
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-23 03:00:41 +0200

    image

--- home.php
+++ home.php
@@ -23,7 +23,7 @@ get_header();
 
             ?>
 
-<section class="section py-4 big-quote-wrapper" style="background-image: url('https://www.liceopitagoracrotone.edu.it/design-scuole-wordpress-theme/assets/css/images'); background-size: cover">
+<section class="section py-4 big-quote-wrapper" style="background-image: url('https://www.liceopitagoracrotone.edu.it/design-scuole-wordpress-theme/assets/css/images/Background.webp'); background-size: cover">
 		<div class="big-quote-bg quote">
 		</div>
 		<div class="container">
