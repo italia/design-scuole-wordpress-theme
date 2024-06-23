b197e66adb5e534c812cb01f696c02b985817807
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-23 02:44:35 +0200

    color quote

--- home.php
+++ home.php
@@ -30,7 +30,7 @@ get_header();
 			<div class="row variable-gutters justify-content-center">
 				<div class="col-md-10">
 					<div class="big-quote">
-						<h2 style="font-size: 5rem">Da sempre una realtà differente nel sistema scolastico italiano</h2>
+						<h2 style="font-size: 5rem" style="color: #EA7653">Da sempre una realtà differente nel sistema scolastico italiano</h2>
 					</div><!-- /big-quote -->
 				</div><!-- /col-md-10 -->
 			</div><!-- /row -->
