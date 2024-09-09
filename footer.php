978c2d991b628a64a454c1941107839942de8451
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2024-09-09 11:22:00 +0200

    Rimosso h2 da nome istituto nel footer

--- footer.php
+++ footer.php
@@ -20,11 +20,11 @@
                 <div class="logo-footer">
                     <a href="<?php echo home_url(); ?>" aria-label="Vai alla homepage" title="Vai alla homepage" class="logo-header">
                     <?php get_template_part("template-parts/common/logo", null, array( 'ignora_stemma_scuola' => true )); ?>
-                    <h2 class="h1">     
+                    <span class="h1">     
                             <span><?php echo dsi_get_option("tipologia_scuola"); ?></span>
                             <span><strong><?php echo dsi_get_option("nome_scuola"); ?></strong></span>
                             <span><?php echo dsi_get_option("luogo_scuola"); ?></span>
-                    </h2>
+                    </span>
                     </a>
                 </div><!-- /logo-footer -->
             </div><!-- /col -->
