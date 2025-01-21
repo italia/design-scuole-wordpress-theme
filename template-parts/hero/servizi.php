6027692cdf74fbc6942f58d65a74f382a1eea346
Sezione Sviluppo di Robyone <sviluppoRobyone@users.noreply.github.com>
2025-01-21 10:52:27 +0100

    fix: bug in oval "servizi" and "argomenti"
    Co-authored-by: Marco Rubin <posta@marcorubin.it>

--- template-parts/hero/servizi.php
+++ template-parts/hero/servizi.php
@@ -8,8 +8,8 @@ $testo_servizi = dsi_get_option("testo_servizi", "servizi");
 	<div class="purple-oval-forms">
 		<svg width="100%" height="100%" viewBox="0 0 578 359" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" role="img" aria-label="purple oval forms">
               <g id="Group-2_Servizi">
-	              <path id="Oval-2_Servizi" d="M578,359c0,-159.61 -129.39,-289 -289,-289c-159.61,0 -289,129.39 -289,289l578,0Z" style="fill:url(#_Linear1);fill-rule:nonzero;"/>
-	              <path id="Oval-2-Copy_Servizi" d="M578,0c0,159.61 -129.39,289 -289,289c-159.61,0 -289,-129.39 -289,-289l578,0Z" style="fill:url(#_Linear2);fill-rule:nonzero;"/>
+	              <path id="Oval-2_Servizi" d="M578,359c0,-159.61 -129.39,-289 -289,-289c-159.61,0 -289,129.39 -289,289l578,0Z" style="fill:url(#_Linear1_Servizi);fill-rule:nonzero;"/>
+	              <path id="Oval-2-Copy_Servizi" d="M578,0c0,159.61 -129.39,289 -289,289c-159.61,0 -289,-129.39 -289,-289l578,0Z" style="fill:url(#_Linear2_Servizi);fill-rule:nonzero;"/>
               </g>
 			<defs>
 				<linearGradient id="_Linear1_Servizi" x1="0" y1="0" x2="1" y2="0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1.76961e-14,289,-289,1.76961e-14,289,70)">
